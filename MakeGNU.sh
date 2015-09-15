cfdb37112e493b5040d3184e2fc762585d4b6e7f
nicolasdanet <nicolas.danet@free.fr>
2015-09-15 13:02:15 +0200

    ...

--- MakeGNU.sh
+++ MakeGNU.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-# "PureData" folder automagically filled.
+# "PureData" folder automagically created.
 # 
 #   ./MakeGNU.sh
 
@@ -12,7 +12,7 @@ rep=$(pwd)
 
 [ "${0%/*}" = "." ] || { echo >&2 "${0##*/}: Must be executed at the same level"; exit 1; }
 
-# Depedencies (http://stackoverflow.com/a/22592801 ).
+# Depedencies ( http://stackoverflow.com/a/22592801 ).
 
 isAlsa=$(dpkg-query -W -f='${Status}' libasound2 2>/dev/null | grep -c "ok installed")
 isTcl_5=$(dpkg-query -W -f='${Status}' tcl8.5-dev 2>/dev/null | grep -c "ok installed")
@@ -21,11 +21,8 @@ isTcl_6=$(dpkg-query -W -f='${Status}' tcl8.6-dev 2>/dev/null | grep -c "ok inst
 isTk_6=$(dpkg-query -W -f='${Status}' tk8.6-dev 2>/dev/null | grep -c "ok installed")
 isJack=$(dpkg-query -W -f='${Status}' libjack-dev 2>/dev/null | grep -c "ok installed")
 
-isTcl=$((${isTcl_5}+${isTcl_6}))
-isTk=$((${isTk_5}+${isTk_6}))
-
-[ isTk -ne 0 ] || { echo >&2 "${0##*/}: tk8.*-dev package required"; exit 1; }
-[ isTcl -ne 0 ] || { echo >&2 "${0##*/}: tcl8.*-dev package required"; exit 1; }
-[ isAlsa -eq 1 ] || { echo >&2 "${0##*/}: libasound2 package required"; exit 1; }
+[ ${isTk_5} -eq 1 ] || [ ${isTk_6} -eq 1 ] || { echo >&2 "${0##*/}: tk8.*-dev package required"; exit 1; }
+[ ${isTcl_5} -eq 1 ] || [ ${isTcl_6} -eq 1 ] || { echo >&2 "${0##*/}: tcl8.*-dev package required"; exit 1; }
+[ ${isAlsa} -eq 1 ] || { echo >&2 "${0##*/}: libasound2 package required"; exit 1; }
 
 echo "SUCCEEDED"
