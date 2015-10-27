80739d121289b22997c015e613f1498fd667fe93
nicolasdanet <nicolas.danet@free.fr>
2015-10-27 17:56:43 +0100

    ...

--- tcl/pd-gui.tcl
+++ tcl/pd-gui.tcl
@@ -268,7 +268,7 @@ proc initialize {audioAPIs midiAPIs fontFamily fontWeight} {
 
     # Initialize some packages.
     
-    foreach module {pd_menu pd_console pd_bind pd_patch} { ::${module}::initialize }
+    foreach module {pd_menu pd_console pd_bind pd_patch} { [format "::%s::initialize" $module] }
     
     # Set the menubar configuration.
     
