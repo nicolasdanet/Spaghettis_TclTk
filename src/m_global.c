acd56b46016f0af392673520652574d52a76a52a
nicolasdanet <nicolas.danet@free.fr>
2016-01-19 16:57:41 +0100

    ...

--- src/m_global.c
+++ src/m_global.c
@@ -33,33 +33,29 @@ void global_default (t_pd *x, t_symbol *s, int argc, t_atom *argv)
 void global_initialize (void)
 {
     t_class *c = class_new (gensym ("pd"), NULL, NULL, sizeof (t_pd), CLASS_DEFAULT, A_NULL);
+
+    class_addMethod (c, (t_method)global_new,               gensym ("new"),  A_SYMBOL, A_SYMBOL, A_NULL);
+    class_addMethod (c, (t_method)buffer_openFile,          gensym ("open"), A_SYMBOL, A_SYMBOL, A_NULL);
+    class_addMethod (c, (t_method)global_dsp,               gensym ("dsp"),  A_GIMME, A_NULL);
+    class_addMethod (c, (t_method)global_key,               gensym ("key"),  A_GIMME, A_NULL);
+    class_addMethod (c, (t_method)global_quit,              gensym ("quit"), A_NULL);
+    class_addMethod (c, (t_method)global_gui,               gensym ("_gui"), A_GIMME, A_NULL);
     
-    /* Public. */
-    
-    class_addMethod (c, (t_method)global_new,       gensym ("new"),  A_SYMBOL, A_SYMBOL, A_NULL);
-    class_addMethod (c, (t_method)buffer_openFile,  gensym ("open"), A_SYMBOL, A_SYMBOL, A_NULL);
-    class_addMethod (c, (t_method)global_dsp,       gensym ("dsp"),  A_GIMME, A_NULL);
-    class_addMethod (c, (t_method)global_key,       gensym ("key"),  A_GIMME, A_NULL);
-    class_addMethod (c, (t_method)global_quit,      gensym ("quit"), A_NULL);
-    
-    /* Private. */
-    
-    class_addMethod (c, (t_method)global_gui,             gensym ("_gui"),              A_GIMME, A_NULL);
-    class_addMethod (c, (t_method)global_audioProperties, gensym ("_audioProperties"),  A_DEFFLOAT, A_NULL);
-    class_addMethod (c, (t_method)global_midiProperties,  gensym ("_midiProperties"),   A_DEFFLOAT, A_NULL);
-    class_addMethod (c, (t_method)global_audioDialog,     gensym ("_audioDialog"),      A_GIMME, A_NULL);
-    class_addMethod (c, (t_method)global_midiDialog,      gensym ("_midiDialog"),       A_GIMME, A_NULL);
-    class_addMethod (c, (t_method)global_audioAPI,        gensym ("_audioAPI"),         A_FLOAT, A_NULL);
-    class_addMethod (c, (t_method)global_midiAPI,         gensym ("_midiAPI"),          A_FLOAT, A_NULL);
-    class_addMethod (c, (t_method)global_setPath,         gensym ("_path"),             A_GIMME, A_NULL);
-    class_addMethod (c, (t_method)global_pathDialog,      gensym ("_pathDialog"),       A_NULL);
-    class_addMethod (c, (t_method)global_shouldQuit,      gensym ("_quit"),             A_NULL);
-    class_addMethod (c, (t_method)global_ping,            gensym ("_ping"),             A_NULL);
-    class_addMethod (c, (t_method)global_savePreferences, gensym ("_savePreferences"),  A_NULL);
+    class_addMethod (c, (t_method)global_audioProperties,   gensym ("_audioProperties"), A_DEFFLOAT, A_NULL);
+    class_addMethod (c, (t_method)global_midiProperties,    gensym ("_midiProperties"),  A_DEFFLOAT, A_NULL);
+    class_addMethod (c, (t_method)global_audioDialog,       gensym ("_audioDialog"),     A_GIMME, A_NULL);
+    class_addMethod (c, (t_method)global_midiDialog,        gensym ("_midiDialog"),      A_GIMME, A_NULL);
+    class_addMethod (c, (t_method)global_audioAPI,          gensym ("_audioAPI"),        A_FLOAT, A_NULL);
+    class_addMethod (c, (t_method)global_midiAPI,           gensym ("_midiAPI"),         A_FLOAT, A_NULL);
+    class_addMethod (c, (t_method)global_setPath,           gensym ("_path"),            A_GIMME, A_NULL);
+    class_addMethod (c, (t_method)global_pathDialog,        gensym ("_pathDialog"),      A_NULL);
+    class_addMethod (c, (t_method)global_shouldQuit,        gensym ("_quit"),            A_NULL);
+    class_addMethod (c, (t_method)global_ping,              gensym ("_ping"),            A_NULL);
+    class_addMethod (c, (t_method)global_savePreferences,   gensym ("_savePreferences"), A_NULL);
     
     #if (PD_LINUX || PD_BSD)
     
-    class_addMethod (c, (t_method)global_watchdog,        gensym ("_watchdog"),         A_NULL);
+    class_addMethod (c, (t_method)global_watchdog, gensym ("_watchdog"), A_NULL);
         
     #endif
 
