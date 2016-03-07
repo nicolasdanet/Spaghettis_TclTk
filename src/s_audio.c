d9150c803207c4fa533da26780b1303f7d0e36ce
nicolasdanet <nicolas.danet@free.fr>
2016-03-07 10:58:09 +0100

    ...

--- src/s_audio.c
+++ src/s_audio.c
@@ -83,20 +83,25 @@ void audio_initializeMemoryAndParameters (int usedChannelsIn, int usedChannelsOu
 {
     /* Channels disposed as consecutive chunk of samples with INTERNAL_BLOCKSIZE length. */
     
-    int m = (usedChannelsIn ? usedChannelsIn : 2) * (INTERNAL_BLOCKSIZE * sizeof (t_sample));
-    int n = (usedChannelsOut ? usedChannelsOut : 2) * (INTERNAL_BLOCKSIZE * sizeof (t_sample));
-
     PD_ASSERT (usedChannelsIn >= 0);
     PD_ASSERT (usedChannelsOut >= 0);
     
+    if (usedChannelsIn != audio_channelsIn || usedChannelsOut != audio_channelsOut) {
+    //
+    int m = (INTERNAL_BLOCKSIZE * sizeof (t_sample)) * (usedChannelsIn ? usedChannelsIn : 2);
+    int n = (INTERNAL_BLOCKSIZE * sizeof (t_sample)) * (usedChannelsOut ? usedChannelsOut : 2);
+
     if (audio_soundIn)  { PD_MEMORY_FREE (audio_soundIn);  audio_soundIn  = NULL; }
     if (audio_soundOut) { PD_MEMORY_FREE (audio_soundOut); audio_soundOut = NULL; }
     
     audio_soundIn  = (t_sample *)PD_MEMORY_GET (m);
     audio_soundOut = (t_sample *)PD_MEMORY_GET (n);
     
-    audio_channelsIn        = usedChannelsIn;
-    audio_channelsOut       = usedChannelsOut;
+    audio_channelsIn  = usedChannelsIn;
+    audio_channelsOut = usedChannelsOut;
+    //
+    }
+    
     audio_sampleRate        = (t_float)sampleRate;
     audio_advanceInSamples  = MICROSECONDS_TO_SECONDS (audio_advanceInMicroseconds * audio_sampleRate);
     audio_advanceInSamples  = PD_MAX (audio_advanceInSamples, INTERNAL_BLOCKSIZE);
