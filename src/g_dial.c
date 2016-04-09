d6e3b8607e0b284422cdd611abfafa7549b9be80
nicolasdanet <nicolas.danet@free.fr>
2016-04-09 17:19:24 +0200

    ...

--- src/g_dial.c
+++ src/g_dial.c
@@ -562,7 +562,7 @@ static void dial_behaviorProperties (t_gobj *z, t_glist *owner)
             x->x_gui.iem_loadbang,
             x->x_steps,
             names.n_unexpandedSend->s_name, names.n_unexpandedReceive->s_name,
-            names.n_unexpandedLabel, x->x_gui.iem_labelX, x->x_gui.iem_labelY,
+            names.n_unexpandedLabel->s_name, x->x_gui.iem_labelX, x->x_gui.iem_labelY,
             x->x_gui.iem_fontSize,
             x->x_gui.iem_colorBackground, x->x_gui.iem_colorForeground, x->x_gui.iem_colorLabel);
     
