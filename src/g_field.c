1870274dd465e3192d9b396fe225f4eac7c0cdc7
nicolasdanet <nicolas.danet@free.fr>
2016-07-16 07:32:48 +0200

    ...

--- src/g_field.c
+++ src/g_field.c
@@ -22,6 +22,48 @@
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
+static t_error field_setAsFloatVariableParsed (t_fielddescriptor *fd,
+    t_symbol *s, 
+    char *firstOpeningParenthesis)
+{
+    char *t[PD_STRING] = { 0 };
+    
+    t_error err = string_append (t, PD_STRING, s->s_name, (int)(firstOpeningParenthesis - s->s_name));
+    
+    if (!err) {
+    //
+    double a, b, c, d, e;
+    int k;
+        
+    fd->fd_un.fd_varname = gensym (t);
+    
+    k = sscanf (firstOpeningParenthesis, "(%lf:%lf)(%lf:%lf)(%lf)", &a, &b, &c, &d, &e);
+            
+    fd->fd_v1       = 0.0;
+    fd->fd_v2       = 0.0;
+    fd->fd_screen1  = 0.0;
+    fd->fd_screen2  = 0.0;
+    fd->fd_quantum  = 0.0;
+    
+    if (k == 2) { fd->fd_v1 = a; fd->fd_v2 = b; fd->fd_screen1 = a; fd->fd_screen2 = b; }
+    else if (k == 4 || k == 5) { 
+        fd->fd_v1 = a; fd->fd_v2 = b; fd->fd_screen1 = c; fd->fd_screen2 = d;
+        if (k == 5) {
+            fd->fd_quantum = e;
+        }
+    }
+    //
+    }
+    
+    PD_ASSERT (!err);
+    
+    return err;
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
 void field_setAsFloatConstant (t_fielddescriptor *fd, t_float f)
 {
     fd->fd_type         = DATA_FLOAT;
@@ -34,55 +76,28 @@ void field_setAsFloatConstant (t_fielddescriptor *fd, t_float f)
     fd->fd_quantum      = 0.0;
 }
 
-void field_setAsFloatVariable(t_fielddescriptor *fd, t_symbol *s)
+void field_setAsFloatVariable (t_fielddescriptor *fd, t_symbol *s)
 {
-    char *s1, *s2, *s3, strbuf[PD_STRING];
-    int i;
-    fd->fd_type = DATA_FLOAT;
-    fd->fd_isVariable = 1;
-    if (!(s1 = strchr(s->s_name, '(')) || !(s2 = strchr(s->s_name, ')'))
-        || (s1 > s2))
-    {
-        fd->fd_un.fd_varname = s;
-        fd->fd_v1 = fd->fd_v2 = fd->fd_screen1 = fd->fd_screen2 =
-            fd->fd_quantum = 0;
-    }
-    else
-    {
-        int cpy = s1 - s->s_name, got;
-        double v1, v2, screen1, screen2, quantum;
-        if (cpy > PD_STRING-5)
-            cpy = PD_STRING-5;
-        strncpy(strbuf, s->s_name, cpy);
-        strbuf[cpy] = 0;
-        fd->fd_un.fd_varname = gensym (strbuf);
-        got = sscanf(s1, "(%lf:%lf)(%lf:%lf)(%lf)",
-            &v1, &v2, &screen1, &screen2,
-                &quantum);
-        fd->fd_v1=v1;
-        fd->fd_v2=v2;
-        fd->fd_screen1=screen1;
-        fd->fd_screen2=screen2;
-        fd->fd_quantum=quantum;
-        if (got < 2)
-            goto fail;
-        if (got == 3 || (got < 4 && strchr(s2, '(')))
-            goto fail;
-        if (got < 5 && (s3 = strchr(s2, '(')) && strchr(s3+1, '('))
-            goto fail;
-        if (got == 4)
-            fd->fd_quantum = 0;
-        else if (got == 2)
-        {
-            fd->fd_quantum = 0;
-            fd->fd_screen1 = fd->fd_v1;
-            fd->fd_screen2 = fd->fd_v2;
-        }
-        return;
-    fail:
-        post("parse error: %s", s->s_name);
-        fd->fd_v1 = fd->fd_screen1 = fd->fd_v2 = fd->fd_screen2 =
-            fd->fd_quantum = 0;
+    char *firstOpeningParenthesis = strchr (s->s_name, '(');
+    char *firstClosingParenthesis = strchr (s->s_name, ')');
+
+    int parse = 1;
+    
+    parse &= (firstOpeningParenthesis != NULL);
+    parse &= (firstClosingParenthesis != NULL);
+    parse &= (firstClosingParenthesis > firstOpeningParenthesis);
+    
+    fd->fd_type         = DATA_FLOAT;
+    fd->fd_isVariable   = 1;
+    
+    if (parse && !field_setAsFloatVariableParsed (fd, s, firstOpeningParenthesis)) { }
+    else {
+        fd->fd_un.fd_varname    = s;
+        fd->fd_v1               = 0.0;
+        fd->fd_v2               = 0.0;
+        fd->fd_screen1          = 0.0;
+        fd->fd_screen2          = 0.0; 
+        fd->fd_quantum          = 0.0;
     }
 }
 
@@ -115,10 +130,7 @@ void field_setAsArray (t_fielddescriptor *fd, int argc, t_atom *argv)
 // -----------------------------------------------------------------------------------------------------------
 #pragma mark -
 
-    /* getting and setting values via fielddescs -- note confusing names;
-    the above are setting up the fielddesc itself. */
-t_float fielddesc_getfloat(t_fielddescriptor *f, t_template *template,
-    t_word *wp, int loud)
+t_float fielddesc_getfloat(t_fielddescriptor *f, t_template *template, t_word *wp, int loud)
 {
     if (f->fd_type == DATA_FLOAT)
     {
