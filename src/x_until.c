3ce5b90bfd9a550d11d5d8e8cd3f2a73fa72f66b
nicolasdanet <nicolas.danet@free.fr>
2016-10-03 10:48:54 +0200

    ...

--- src/x_until.c
+++ src/x_until.c
@@ -12,56 +12,87 @@
 #include "m_pd.h"
 #include "m_core.h"
 #include "m_macros.h"
-#include "g_graphics.h"
 
-static t_class *until_class;
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-typedef struct _until
+static t_class *until_class;    /* Shared. */
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+typedef struct _until {
+    t_object    x_obj;          /* Must be the first. */
+    int         x_run;
+    int         x_count;
+    t_outlet    *x_outlet;
+    } t_until;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void until_perform (t_until *x)
 {
-    t_object x_obj;
-    int x_run;
-    int x_count;
-} t_until;
+    while (x->x_run && x->x_count) { x->x_count--; outlet_bang (x->x_outlet); }
+}
 
-static void *until_new(void)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void until_bang (t_until *x)
 {
-    t_until *x = (t_until *)pd_new(until_class);
-    inlet_new(&x->x_obj, &x->x_obj.te_g.g_pd, &s_bang, sym_inlet2);
-    outlet_new(&x->x_obj, &s_bang);
+    x->x_run = 1; x->x_count = -1; until_perform (x);
+}
+
+static void until_float (t_until *x, t_float f)
+{
+    x->x_run = 1; x->x_count = PD_MAX (0.0, f); until_perform (x);
+}
+
+static void until_stop (t_until *x)
+{
     x->x_run = 0;
-    return (x);
 }
 
-static void until_bang(t_until *x)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void *until_new (void)
 {
-    x->x_run = 1;
-    x->x_count = -1;
-    while (x->x_run && x->x_count)
-        x->x_count--, outlet_bang(x->x_obj.te_outlet);
+    t_until *x = (t_until *)pd_new (until_class);
+    
+    x->x_run    = 0;
+    x->x_outlet = outlet_new (cast_object (x), &s_bang);
+    
+    inlet_new (cast_object (x), cast_pd (x), &s_bang, sym_inlet2);
+
+    return x;
 }
 
-static void until_float(t_until *x, t_float f)
-{
-    if (f < 0)
-        f = 0;
-    x->x_run = 1;
-    x->x_count = f;
-    while (x->x_run && x->x_count)
-        x->x_count--, outlet_bang(x->x_obj.te_outlet);
-}
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
 
-static void until_bang2(t_until *x)
+void until_setup (void)
 {
-    x->x_run = 0;
-}
-
-void until_setup(void)
-{
-    until_class = class_new(sym_until, (t_newmethod)until_new, 0,
-        sizeof(t_until), 0, 0);
-    class_addBang(until_class, until_bang);
-    class_addFloat(until_class, until_float);
-    class_addMethod(until_class, (t_method)until_bang2, sym_inlet2, 0);
+    t_class *c = NULL;
+    
+    c = class_new (sym_until,
+            (t_newmethod)until_new,
+            NULL,
+            sizeof (t_until),
+            CLASS_DEFAULT,
+            A_NULL);
+            
+    class_addBang (c, until_bang);
+    class_addFloat (c, until_float);
+    
+    class_addMethod (c, until_stop, sym_inlet2, A_NULL);
+    
+    until_class = c;
 }
 
 // -----------------------------------------------------------------------------------------------------------
