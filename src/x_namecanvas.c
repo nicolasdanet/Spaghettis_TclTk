e1ec2f757b6a6bf489a87d194d4004f0f7b544c2
nicolasdanet <nicolas.danet@free.fr>
2016-10-10 08:04:50 +0200

    ...

--- src/x_namecanvas.c
+++ src/x_namecanvas.c
@@ -12,37 +12,61 @@
 #include "m_pd.h"
 #include "m_core.h"
 #include "m_macros.h"
-#include "m_alloca.h"
-#include "s_system.h"
 #include "g_graphics.h"
 
-/* ------------- namecanvas (delete this later) --------------------- */
-static t_class *namecanvas_class;
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-typedef struct _namecanvas
-{
-    t_object x_obj;
-    t_symbol *x_sym;
-    t_pd *x_owner;
-} t_namecanvas;
+static t_class *namecanvas_class;           /* Shared. */
 
-static void *namecanvas_new(t_symbol *s)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+typedef struct _namecanvas {
+    t_object    x_obj;                      /* Must be the first. */
+    t_symbol    *x_name;
+    t_glist     *x_owner;
+    } t_namecanvas;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void *namecanvas_new (t_symbol *s)
 {
-    t_namecanvas *x = (t_namecanvas *)pd_new(namecanvas_class);
-    x->x_owner = (t_pd *)canvas_getCurrent();
-    x->x_sym = s;
-    if (*s->s_name) pd_bind(x->x_owner, s);
-    return (x);
+    t_namecanvas *x = (t_namecanvas *)pd_new (namecanvas_class);
+    
+    x->x_owner = canvas_getCurrent();
+    x->x_name = s;
+    
+    if (x->x_name != &s_) { pd_bind (cast_pd (x->x_owner), x->x_name); }
+    
+    return x;
 }
 
-static void namecanvas_free(t_namecanvas *x)
+static void namecanvas_free (t_namecanvas *x)
 {
-    if (*x->x_sym->s_name) pd_unbind(x->x_owner, x->x_sym);
+    if (x->x_name != &s_) { pd_unbind (cast_pd (x->x_owner), x->x_name); }
 }
 
-void namecanvas_setup(void)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void namecanvas_setup (void)
 {
-    namecanvas_class = class_new(sym_namecanvas,
-        (t_newmethod)namecanvas_new, (t_method)namecanvas_free,
-            sizeof(t_namecanvas), CLASS_NOINLET, A_DEFSYMBOL, 0);
+    t_class *c = NULL;
+    
+    c = class_new (sym_namecanvas,
+            (t_newmethod)namecanvas_new,
+            (t_method)namecanvas_free,
+            sizeof (t_namecanvas),
+            CLASS_DEFAULT | CLASS_NOINLET,
+            A_DEFSYMBOL,
+            A_NULL);
+        
+    namecanvas_class = c;
 }
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
