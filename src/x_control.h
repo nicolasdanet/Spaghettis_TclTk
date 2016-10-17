bf77e4e3179f7c3aa57bafcfe3f9e6ba1d56b7f3
nicolasdanet <nicolas.danet@free.fr>
2016-10-17 17:09:17 +0200

    ...

--- src/x_control.h
+++ src/x_control.h
@@ -115,10 +115,10 @@ typedef struct _atomoutlet {
 
 #define OSC_ROUNDUP(x)                      (((x) + 3) & (~3))
 
-#define OSC_READ(x)                         ((((int)(((x) + 0)->a_w.w_float)) & 0xff) << 24) | \
-                                            ((((int)(((x) + 1)->a_w.w_float)) & 0xff) << 16) | \
-                                            ((((int)(((x) + 2)->a_w.w_float)) & 0xff) << 8)  | \
-                                            ((((int)(((x) + 3)->a_w.w_float)) & 0xff) << 0)
+#define OSC_READ(x)                         ((((int)(GET_FLOAT ((x) + 0))) & 0xff) << 24) | \
+                                            ((((int)(GET_FLOAT ((x) + 1))) & 0xff) << 16) | \
+                                            ((((int)(GET_FLOAT ((x) + 2))) & 0xff) << 8)  | \
+                                            ((((int)(GET_FLOAT ((x) + 3))) & 0xff) << 0)
 
 #define OSC_WRITE(x, i)                     SET_FLOAT ((x) + 0, (((i) >> 24) & 0xff)); \
                                             SET_FLOAT ((x) + 1, (((i) >> 16) & 0xff)); \
