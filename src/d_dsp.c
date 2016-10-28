699b0fa5aef35cd71e164aef1c853e7e9071aafe
nicolasdanet <nicolas.danet@free.fr>
2016-10-28 10:30:41 +0200

    ...

--- src/d_dsp.c
+++ src/d_dsp.c
@@ -27,20 +27,38 @@ t_int dsp_done (t_int *w)
     return 0;
 }
 
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
 void dsp_add (t_perform f, int n, ...)
 {
-    int newsize = pd_this->pd_dspChainSize + n+1, i;
+    int size = pd_this->pd_dspChainSize + n + 1;
+    
+    size_t newSize = sizeof (t_int) * size;
+    size_t oldSize = sizeof (t_int) * pd_this->pd_dspChainSize;
+    
+    pd_this->pd_dspChain = PD_MEMORY_RESIZE (pd_this->pd_dspChain, oldSize, newSize);
+    
+    {
+    //
+    int i;
     va_list ap;
 
-    pd_this->pd_dspChain = PD_MEMORY_RESIZE(pd_this->pd_dspChain, 
-        pd_this->pd_dspChainSize * sizeof (t_int), newsize * sizeof (t_int));
-    pd_this->pd_dspChain[pd_this->pd_dspChainSize-1] = (t_int)f;
-    va_start(ap, n);
-    for (i = 0; i < n; i++)
-        pd_this->pd_dspChain[pd_this->pd_dspChainSize + i] = va_arg(ap, t_int);
-    va_end(ap);
-    pd_this->pd_dspChain[newsize-1] = (t_int)dsp_done;
-    pd_this->pd_dspChainSize = newsize;
+    pd_this->pd_dspChain[pd_this->pd_dspChainSize - 1] = (t_int)f;
+
+    va_start (ap, n);
+    
+    for (i = 0; i < n; i++) { 
+        pd_this->pd_dspChain[pd_this->pd_dspChainSize + i] = va_arg (ap, t_int);
+    }
+    
+    va_end (ap);
+    //
+    }
+    
+    pd_this->pd_dspChain[size - 1] = (t_int)dsp_done;
+    pd_this->pd_dspChainSize = size;
 }
 
 // -----------------------------------------------------------------------------------------------------------
