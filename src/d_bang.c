0e321d573d5e010503e2e121f24021f8d4ba0a79
nicolasdanet <nicolas.danet@free.fr>
2016-11-15 14:40:13 +0100

    ...

--- src/d_bang.c
+++ src/d_bang.c
@@ -1,65 +1,101 @@
-/* Copyright (c) 1997-1999 Miller Puckette.
-* For information on usage and redistribution, and for a DISCLAIMER OF ALL
-* WARRANTIES, see the file, "LICENSE.txt," in this distribution.  */
 
-/*  miscellaneous: print~; more to come.
+/* 
+    Copyright (c) 1997-2016 Miller Puckette and others.
 */
+
+/* < https://opensource.org/licenses/BSD-3-Clause > */
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
 
 #include "m_pd.h"
 #include "m_core.h"
 #include "m_macros.h"
 #include "d_dsp.h"
-#include <stdio.h>
-#include <string.h>
 
-static t_class *bang_tilde_class;
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-typedef struct _bang_tilde
+static t_class *bang_tilde_class;       /* Shared. */
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+typedef struct _bang_tilde {
+    t_object    x_obj;                  /* Must be the first. */
+    t_outlet    *x_outlet;
+    t_clock     *x_clock;
+    } t_bang_tilde;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void bang_tilde_task (t_bang_tilde *x)
 {
-    t_object x_obj;
-    t_clock *x_clock;
-} t_bang_tilde;
+    outlet_bang (x->x_outlet);
+}
 
-static t_int *bang_tilde_perform(t_int *w)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static t_int *bang_tilde_perform (t_int *w)
 {
     t_bang_tilde *x = (t_bang_tilde *)(w[1]);
-    clock_delay(x->x_clock, 0);
-    return (w+2);
+    
+    clock_delay (x->x_clock, 0.0);
+    
+    return (w + 2);
 }
 
-static void bang_tilde_dsp(t_bang_tilde *x, t_signal **sp)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+static void bang_tilde_dsp (t_bang_tilde *x, t_signal **sp)
 {
-    dsp_add(bang_tilde_perform, 1, x);
+    dsp_add (bang_tilde_perform, 1, x);
 }
 
-static void bang_tilde_tick(t_bang_tilde *x)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void *bang_tilde_new (t_symbol *s)
 {
-    outlet_bang(x->x_obj.te_outlet);
+    t_bang_tilde *x = (t_bang_tilde *)pd_new (bang_tilde_class);
+    
+    x->x_outlet = outlet_new (cast_object (x), &s_bang);
+    x->x_clock  = clock_new ((void *)x, (t_method)bang_tilde_task);
+    
+    return x;
 }
 
-static void bang_tilde_free(t_bang_tilde *x)
+static void bang_tilde_free (t_bang_tilde *x)
 {
-    clock_free(x->x_clock);
+    clock_free (x->x_clock);
 }
 
-static void *bang_tilde_new(t_symbol *s)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void bang_tilde_setup (void)
 {
-    t_bang_tilde *x = (t_bang_tilde *)pd_new(bang_tilde_class);
-    x->x_clock = clock_new(x, (t_method)bang_tilde_tick);
-    outlet_new(&x->x_obj, &s_bang);
-    return (x);
+    t_class *c = NULL;
+    
+    c = class_new (sym_bang__tilde__,
+            (t_newmethod)bang_tilde_new,
+            (t_method)bang_tilde_free,
+            sizeof (t_bang_tilde),
+            CLASS_DEFAULT,
+            A_NULL);
+            
+    class_addDSP (c, bang_tilde_dsp);
+    
+    bang_tilde_class = c;
 }
 
-void bang_tilde_setup(void)
-{
-    bang_tilde_class = class_new(sym_bang__tilde__, (t_newmethod)bang_tilde_new,
-        (t_method)bang_tilde_free, sizeof(t_bang_tilde), 0, 0);
-    class_addMethod(bang_tilde_class, (t_method)bang_tilde_dsp,
-        sym_dsp, A_CANT, A_NULL);
-}
-
-
-
-
-
-
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
