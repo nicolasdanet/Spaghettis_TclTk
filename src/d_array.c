c4f36780e0188c208a514d6a4adb80f7739108f0
nicolasdanet <nicolas.danet@free.fr>
2016-11-24 11:07:17 +0100

    ...

--- src/d_array.c
+++ src/d_array.c
@@ -501,41 +501,6 @@ static void tabread4_tilde_setup(void)
 
 /******************** tabosc4~ ***********************/
 
-/* this is all copied from d_osc.c... what include file could this go in? */
-#define UNITBIT32 1572864.  /* 3*2^19; bit 32 has place value 1 */
-
-#if defined(__FreeBSD__) || defined(__APPLE__) || defined(__FreeBSD_kernel__) \
-    || defined(__OpenBSD__)
-#include <machine/endian.h>
-#endif
-
-#if defined(__linux__) || defined(__CYGWIN__) || defined(__GNU__) || \
-    defined(ANDROID)
-#include <endian.h>
-#endif
-
-#ifdef __MINGW32__
-#include <sys/param.h>
-#endif
-
-#ifdef _MSC_VER
-/* _MSVC lacks BYTE_ORDER and LITTLE_ENDIAN */
-#define LITTLE_ENDIAN 0x0001
-#define BYTE_ORDER LITTLE_ENDIAN
-#endif
-
-#if !defined(BYTE_ORDER) || !defined(LITTLE_ENDIAN)                         
-#error No byte order defined                                                    
-#endif
-
-#if BYTE_ORDER == LITTLE_ENDIAN
-# define HIOFFSET 1                                                              
-# define LOWOFFSET 0                                                             
-#else                                                                           
-# define HIOFFSET 0    /* word offset to find MSB */                             
-# define LOWOFFSET 1    /* word offset to find LSB */                            
-#endif
-
 union tabfudge
 {
     double tf_d;
--- src/d_osc.c
+++ src/d_osc.c
@@ -11,42 +11,8 @@
 #include "math.h"
 #include "d_dsp.h"
 
-#define UNITBIT32 1572864.  /* 3*2^19; bit 32 has place value 1 */
-
 #define LOGCOSTABSIZE       9
 #define COSTABSIZE          (1 << LOGCOSTABSIZE)
-
-#if defined(__FreeBSD__) || defined(__APPLE__) || defined(__FreeBSD_kernel__) \
-    || defined(__OpenBSD__)
-#include <machine/endian.h>
-#endif
-
-#if defined(__linux__) || defined(__CYGWIN__) || defined(__GNU__) || \
-    defined(ANDROID)
-#include <endian.h>
-#endif
-
-#ifdef __MINGW32__
-#include <sys/param.h>
-#endif
-
-#ifdef _MSC_VER
-/* _MSVC lacks BYTE_ORDER and LITTLE_ENDIAN */
-#define LITTLE_ENDIAN 0x0001
-#define BYTE_ORDER LITTLE_ENDIAN
-#endif
-
-#if !defined(BYTE_ORDER) || !defined(LITTLE_ENDIAN) 
-#error No byte order defined                                                    
-#endif
-
-#if BYTE_ORDER == LITTLE_ENDIAN
-# define HIOFFSET 1                                                              
-# define LOWOFFSET 0                                                             
-#else                                                                           
-# define HIOFFSET 0    /* word offset to find MSB */
-# define LOWOFFSET 1    /* word offset to find LSB */
-#endif
 
 union tabfudge
 {
@@ -116,7 +82,7 @@ static void phasor_ft1(t_phasor *x, t_float f)
     x->x_phase = f;
 }
 
-static void phasor_setup(void)
+void phasor_setup(void)
 {
     phasor_class = class_new(sym_phasor__tilde__, (t_newmethod)phasor_new, 0,
         sizeof(t_phasor), 0, A_DEFFLOAT, 0);
@@ -223,7 +189,7 @@ static void cos_tilde_maketable(void)
     if ((unsigned)tf.tf_i[LOWOFFSET] != 0x80000000) { PD_BUG; }
 }
 
-static void cos_tilde_setup(void)
+void cos_tilde_setup(void)
 {
     cos_tilde_class = class_new(sym_cos__tilde__, (t_newmethod)cos_tilde_new, 0,
         sizeof(t_cos), 0, A_DEFFLOAT, 0);
@@ -323,7 +289,7 @@ static void osc_ft1(t_osc *x, t_float f)
     x->x_phase = COSTABSIZE * f;
 }
 
-static void osc_setup(void)
+void osc_setup(void)
 {    
     osc_class = class_new(sym_osc__tilde__, (t_newmethod)osc_new, 0,
         sizeof(t_osc), 0, A_DEFFLOAT, 0);
@@ -497,21 +463,11 @@ static void noise_dsp(t_noise *x, t_signal **sp)
     dsp_add(noise_perform, 3, sp[0]->s_vector, &x->x_val, sp[0]->s_vectorSize);
 }
 
-static void noise_setup(void)
+void noise_setup(void)
 {
     noise_class = class_new(sym_noise__tilde__, (t_newmethod)noise_new, 0,
         sizeof(t_noise), 0, 0);
     class_addMethod(noise_class, (t_method)noise_dsp, sym_dsp, A_CANT, 0);
 }
 
-
-/* ----------------------- global setup routine ---------------- */
-void d_osc_setup(void)
-{
-    phasor_setup();
-    cos_tilde_setup();
-    osc_setup();
-    sigvcf_setup();
-    noise_setup();
-}
 
