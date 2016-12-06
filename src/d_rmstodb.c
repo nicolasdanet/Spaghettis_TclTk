4aff06ddd1746de40c45045cdb4948ef1d453310
nicolasdanet <nicolas.danet@free.fr>
2016-12-06 11:28:12 +0100

    ...

--- src/d_rmstodb.c
+++ src/d_rmstodb.c
@@ -14,50 +14,80 @@
 #include "m_macros.h"
 #include "d_dsp.h"
 
-typedef struct rmstodb_tilde
-{
-    t_object x_obj;
-    t_float x_f;
-} t_rmstodb_tilde;
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-t_class *rmstodb_tilde_class;
+static t_class *rmstodb_tilde_class;        /* Shared. */
 
-static void *rmstodb_tilde_new(void)
-{
-    t_rmstodb_tilde *x = (t_rmstodb_tilde *)pd_new(rmstodb_tilde_class);
-    outlet_new(&x->x_obj, &s_signal);
-    x->x_f = 0;
-    return x;
-}
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-static t_int *rmstodb_tilde_perform(t_int *w)
+typedef struct rmstodb_tilde {
+    t_object    x_obj;                      /* Must be the first. */
+    t_float     x_f;
+    t_outlet    *x_outlet;
+    } t_rmstodb_tilde;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+/* No aliasing. */
+
+static t_int *rmstodb_tilde_perform (t_int *w)
 {
-    t_sample *in = *(t_sample **)(w+1), *out = *(t_sample **)(w+2);
-    t_int n = *(t_int *)(w+3);
-    for (; n--; in++, out++)
-    {
-        t_sample f = *in;
-        if (f <= 0) *out = 0;
-        else
-        {
-            t_sample g = 100 + 20./PD_LOGTEN * log(f);
-            *out = (g < 0 ? 0 : g);
-        }
-    }
+    PD_RESTRICTED in  = (t_sample *)(w[1]);
+    PD_RESTRICTED out = (t_sample *)(w[2]);
+    int n = (int)(w[3]);
+    
+    while (n--) { *out++ = math_rootMeanSquareToDecibel (*in++); }
+    
     return (w + 4);
 }
 
-static void rmstodb_tilde_dsp(t_rmstodb_tilde *x, t_signal **sp)
+static void rmstodb_tilde_dsp (t_rmstodb_tilde *x, t_signal **sp)
 {
-    dsp_add(rmstodb_tilde_perform, 3, sp[0]->s_vector, sp[1]->s_vector, sp[0]->s_vectorSize);
+    PD_ASSERT (sp[0]->s_vector != sp[1]->s_vector);
+    
+    dsp_add (rmstodb_tilde_perform, 3, sp[0]->s_vector, sp[1]->s_vector, sp[0]->s_vectorSize);
 }
 
-void rmstodb_tilde_setup(void)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void *rmstodb_tilde_new (void)
 {
-    rmstodb_tilde_class = class_new(sym_rmstodb__tilde__, 
-        (t_newmethod)rmstodb_tilde_new, 0, sizeof(t_rmstodb_tilde), 0, 0);
-    CLASS_SIGNAL(rmstodb_tilde_class, t_rmstodb_tilde, x_f);
-    class_addMethod(rmstodb_tilde_class, (t_method)rmstodb_tilde_dsp,
-        sym_dsp, A_CANT, 0);
-    class_setHelpName(rmstodb_tilde_class, sym_mtof__tilde__);
+    t_rmstodb_tilde *x = (t_rmstodb_tilde *)pd_new (rmstodb_tilde_class);
+    
+    x->x_outlet = outlet_new (cast_object (x), &s_signal);
+
+    return x;
 }
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void rmstodb_tilde_setup (void)
+{
+    t_class *c = NULL;
+    
+    c = class_new (sym_rmstodb__tilde__, 
+            (t_newmethod)rmstodb_tilde_new,
+            NULL,
+            sizeof (t_rmstodb_tilde),
+            CLASS_DEFAULT,
+            A_NULL);
+            
+    CLASS_SIGNAL (c, t_rmstodb_tilde, x_f);
+    
+    class_addDSP (c, rmstodb_tilde_dsp);
+    
+    class_setHelpName (c, sym_mtof__tilde__);
+    
+    rmstodb_tilde_class = c;
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
