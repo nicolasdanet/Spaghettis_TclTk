bff6e56589eed49c99c7ef2d4520c463352608cd
nicolasdanet <nicolas.danet@free.fr>
2016-12-06 14:59:39 +0100

    ...

--- src/d_exp.c
+++ src/d_exp.c
@@ -14,42 +14,78 @@
 #include "m_macros.h"
 #include "d_dsp.h"
 
-static t_class *exp_tilde_class;
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-typedef struct _exp_tilde
-{
-    t_object x_obj;
-    t_float x_f;
-} t_exp_tilde;
+static t_class *exp_tilde_class;            /* Shared. */
 
-static void *exp_tilde_new( void)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+typedef struct _exp_tilde {
+    t_object    x_obj;                      /* Must be the first. */
+    t_float     x_f;
+    t_outlet    *x_outlet;
+    } t_exp_tilde;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+/* No aliasing. */
+
+t_int *exp_tilde_perform (t_int *w)
 {
-    t_exp_tilde *x = (t_exp_tilde *)pd_new(exp_tilde_class);
-    outlet_new(&x->x_obj, &s_signal);
+    PD_RESTRICTED in1 = (t_sample *)(w[1]);
+    PD_RESTRICTED out = (t_sample *)(w[2]);
+    int n = (int)(w[3]);
+    
+    while (n--) { *out++ = exp (*in1++); }
+    
+    return (w + 4);
+}
+
+static void exp_tilde_dsp (t_exp_tilde *x, t_signal **sp)
+{
+    PD_ASSERT (sp[0]->s_vector != sp[1]->s_vector);
+    
+    dsp_add (exp_tilde_perform, 3, sp[0]->s_vector, sp[1]->s_vector, sp[0]->s_vectorSize);
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void *exp_tilde_new (void)
+{
+    t_exp_tilde *x = (t_exp_tilde *)pd_new (exp_tilde_class);
+    
+    x->x_outlet = outlet_new (cast_object (x), &s_signal);
+    
     return x;
 }
 
-t_int *exp_tilde_perform(t_int *w)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void exp_tilde_setup (void)
 {
-    t_sample *in1 = (t_sample *)(w[1]);
-    t_sample *out = (t_sample *)(w[2]);
-    int n = (int)(w[3]);
-    while (n--)
-        *out++ = exp(*in1++);
-    return (w+4);
+    t_class *c = NULL;
+    
+    c = class_new (sym_exp__tilde__,
+            (t_newmethod)exp_tilde_new,
+            NULL,
+            sizeof (t_exp_tilde),
+            CLASS_DEFAULT,
+            A_NULL);
+            
+    CLASS_SIGNAL (c, t_exp_tilde, x_f);
+    
+    class_addDSP (c, exp_tilde_dsp);
+        
+    exp_tilde_class = c;
 }
 
-static void exp_tilde_dsp(t_exp_tilde *x, t_signal **sp)
-{
-    dsp_add(exp_tilde_perform, 3,
-        sp[0]->s_vector, sp[1]->s_vector, sp[0]->s_vectorSize);
-}
-
-void exp_tilde_setup(void)
-{
-    exp_tilde_class = class_new(sym_exp__tilde__, (t_newmethod)exp_tilde_new, 0,
-        sizeof(t_exp_tilde), 0, 0);
-    CLASS_SIGNAL(exp_tilde_class, t_exp_tilde, x_f);
-    class_addMethod(exp_tilde_class, (t_method)exp_tilde_dsp,
-        sym_dsp, A_CANT, 0);
-}
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
