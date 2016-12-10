0003f2e31450233bd2b4ec41c77f3c19076dc96d
nicolasdanet <nicolas.danet@free.fr>
2016-12-10 10:09:28 +0100

    ...

--- src/d_sig.c
+++ src/d_sig.c
@@ -15,41 +15,72 @@
 #include "s_system.h"
 #include "d_dsp.h"
 
-/* -------------------------- sig~ ------------------------------ */
-static t_class *sig_tilde_class;
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-typedef struct _sig_tilde
-{
-    t_object x_obj;
-    t_float x_f;
-} t_sig;
+static t_class *sig_tilde_class;        /* Shared. */
 
-static void sig_tilde_float(t_sig *x, t_float f)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+typedef struct _sig_tilde {
+    t_object    x_obj;                  /* Must be the first. */
+    t_float     x_f;
+    t_outlet    *x_outlet;
+    } t_sig_tilde;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void sig_tilde_float (t_sig_tilde *x, t_float f)
 {
     x->x_f = f;
 }
 
-static void sig_tilde_dsp(t_sig *x, t_signal **sp)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void sig_tilde_dsp (t_sig_tilde *x, t_signal **sp)
 {
     dsp_addScalarPerform (&x->x_f, sp[0]->s_vector, sp[0]->s_vectorSize);
-    // dsp_add(sig_tilde_perform, 3, &x->x_f, sp[0]->s_vector, sp[0]->s_vectorSize);
 }
 
-static void *sig_tilde_new(t_float f)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void *sig_tilde_new (t_float f)
 {
-    t_sig *x = (t_sig *)pd_new(sig_tilde_class);
-    x->x_f = f;
-    outlet_new(&x->x_obj, &s_signal);
+    t_sig_tilde *x = (t_sig_tilde *)pd_new (sig_tilde_class);
+    
+    x->x_f      = f;
+    x->x_outlet = outlet_new (cast_object (x), &s_signal);
+    
     return x;
 }
 
-void sig_tilde_setup(void)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void sig_tilde_setup (void)
 {
-    sig_tilde_class = class_new(sym_sig__tilde__, (t_newmethod)sig_tilde_new, 0,
-        sizeof(t_sig), 0, A_DEFFLOAT, 0);
-    class_addFloat(sig_tilde_class, (t_method)sig_tilde_float);
-    class_addMethod(sig_tilde_class, (t_method)sig_tilde_dsp,
-        sym_dsp, A_CANT, 0);
+    t_class *c = NULL;
+    
+    c = class_new (sym_sig__tilde__,
+            (t_newmethod)sig_tilde_new,
+            NULL,
+            sizeof (t_sig_tilde),
+            CLASS_DEFAULT, 
+            A_DEFFLOAT,
+            A_NULL);
+    
+    class_addDSP (c, sig_tilde_dsp);
+    class_addFloat (c, sig_tilde_float);
+    
+    sig_tilde_class = c;
 }
 
 // -----------------------------------------------------------------------------------------------------------
