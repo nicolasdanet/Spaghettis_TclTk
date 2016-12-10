a4cd039648768400d0ad73e085d62f90d6504bbc
nicolasdanet <nicolas.danet@free.fr>
2016-12-10 15:12:02 +0100

    ...

--- src/d_line.c
+++ src/d_line.c
@@ -15,152 +15,142 @@
 #include "s_system.h"
 #include "d_dsp.h"
 
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+static t_class *line_tilde_class;       /* Shared. */
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+typedef struct _line_tilde {
+    t_object    x_obj;                  /* Must be the first. */
+    t_sample    x_target;
+    t_sample    x_current;
+    t_sample    x_incrementPerTick;
+    t_sample    x_incrementPerSample;
+    t_float     x_inverseOfVectorSize;
+    t_float     x_millisecondsToTicks;
+    t_float     x_time;
+    t_float     x_timePerformed;
+    int         x_ticksLeft;
+    int         x_retarget;
+    t_outlet    *x_outlet;
+    } t_line_tilde;
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 #pragma mark -
 
-/* -------------------------- line~ ------------------------------ */
-static t_class *line_tilde_class;
-
-typedef struct _line_tilde
+static void line_tilde_float (t_line_tilde *x, t_float f)
 {
-    t_object x_obj;
-    t_sample x_target; /* target value of ramp */
-    t_sample x_value; /* current value of ramp at block-borders */
-    t_sample x_biginc;
-    t_sample x_inc;
-    t_float x_1overn;
-    t_float x_dspticktomsec;
-    t_float x_inletvalue;
-    t_float x_inletwas;
-    int x_ticksleft;
-    int x_retarget;
-} t_line_tilde;
+    if (x->x_time <= 0) {
+    
+        x->x_target         = f;
+        x->x_current        = f;
+        x->x_ticksLeft      = 0;
+        x->x_retarget       = 0;
+        
+    } else {
+    
+        x->x_target         = f;
+        x->x_retarget       = 1;
+        x->x_timePerformed  = x->x_time;
+        x->x_time           = 0.0;
+    }
+}
 
-static t_int *line_tilde_perform(t_int *w)
+static void line_tilde_stop (t_line_tilde *x)
+{
+    x->x_target     = x->x_current;
+    x->x_ticksLeft  = 0;
+    x->x_retarget   = 0;
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+/* No aliasing. */
+
+static t_int *line_tilde_perform (t_int *w)
 {
     t_line_tilde *x = (t_line_tilde *)(w[1]);
-    t_sample *out = (t_sample *)(w[2]);
+    PD_RESTRICTED out = (t_sample *)(w[2]);
     int n = (int)(w[3]);
-    t_sample f = x->x_value;
-
-    if (PD_BIG_OR_SMALL(f))
-            x->x_value = f = 0;
-    if (x->x_retarget)
-    {
-        int nticks = x->x_inletwas * x->x_dspticktomsec;
-        if (!nticks) nticks = 1;
-        x->x_ticksleft = nticks;
-        x->x_biginc = (x->x_target - x->x_value)/(t_float)nticks;
-        x->x_inc = x->x_1overn * x->x_biginc;
-        x->x_retarget = 0;
+    
+    t_sample f;
+    
+    if (x->x_retarget) {
+    
+        int numberOfTicks = PD_MAX (1, (int)(x->x_timePerformed * x->x_millisecondsToTicks));
+        
+        x->x_ticksLeft          = numberOfTicks;
+        x->x_incrementPerTick   = (x->x_target - x->x_current) / (t_float)(numberOfTicks);
+        x->x_incrementPerSample = x->x_incrementPerTick * x->x_inverseOfVectorSize;
+        x->x_retarget           = 0;
     }
-    if (x->x_ticksleft)
-    {
-        t_sample f = x->x_value;
-        while (n--) *out++ = f, f += x->x_inc;
-        x->x_value += x->x_biginc;
-        x->x_ticksleft--;
+    
+    if (x->x_ticksLeft) {
+    
+        f = x->x_current;
+        while (n--) { *out++ = f; f += x->x_incrementPerSample; }
+        x->x_current += x->x_incrementPerTick; x->x_ticksLeft--;
+        
+    } else {
+    
+        f = x->x_target;
+        while (n--) { *out++ = f; }
+        x->x_current = f;
     }
-    else
-    {
-        t_sample g = x->x_value = x->x_target;
-        while (n--)
-            *out++ = g;
-    }
-    return (w+4);
+    
+    return (w + 4);
 }
 
-/* TB: vectorized version */
-static t_int *line_tilde_perf8(t_int *w)
+static void line_tilde_dsp (t_line_tilde *x, t_signal **sp)
 {
-    t_line_tilde *x = (t_line_tilde *)(w[1]);
-    t_sample *out = (t_sample *)(w[2]);
-    int n = (int)(w[3]);
-    t_sample f = x->x_value;
-
-    if (PD_BIG_OR_SMALL(f))
-        x->x_value = f = 0;
-    if (x->x_retarget)
-    {
-        int nticks = x->x_inletwas * x->x_dspticktomsec;
-        if (!nticks) nticks = 1;
-        x->x_ticksleft = nticks;
-        x->x_biginc = (x->x_target - x->x_value)/(t_sample)nticks;
-        x->x_inc = x->x_1overn * x->x_biginc;
-        x->x_retarget = 0;
-    }
-    if (x->x_ticksleft)
-    {
-        t_sample f = x->x_value;
-        while (n--) *out++ = f, f += x->x_inc;
-        x->x_value += x->x_biginc;
-        x->x_ticksleft--;
-    }
-    else
-    {
-        t_sample f = x->x_value = x->x_target;
-        for (; n; n -= 8, out += 8)
-        {
-            out[0] = f; out[1] = f; out[2] = f; out[3] = f; 
-            out[4] = f; out[5] = f; out[6] = f; out[7] = f;
-        }
-    }
-    return (w+4);
+    x->x_inverseOfVectorSize = 1.0 / sp[0]->s_vectorSize;
+    x->x_millisecondsToTicks = sp[0]->s_sampleRate / (1000.0 * sp[0]->s_vectorSize);
+    
+    dsp_add (line_tilde_perform, 3, x, sp[0]->s_vector, sp[0]->s_vectorSize);
 }
 
-static void line_tilde_float(t_line_tilde *x, t_float f)
-{
-    if (x->x_inletvalue <= 0)
-    {
-        x->x_target = x->x_value = f;
-        x->x_ticksleft = x->x_retarget = 0;
-    }
-    else
-    {
-        x->x_target = f;
-        x->x_retarget = 1;
-        x->x_inletwas = x->x_inletvalue;
-        x->x_inletvalue = 0;
-    }
-}
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
 
-static void line_tilde_stop(t_line_tilde *x)
+static void *line_tilde_new (void)
 {
-    x->x_target = x->x_value;
-    x->x_ticksleft = x->x_retarget = 0;
-}
-
-static void line_tilde_dsp(t_line_tilde *x, t_signal **sp)
-{
-    if(sp[0]->s_vectorSize&7)
-        dsp_add(line_tilde_perform, 3, x, sp[0]->s_vector, sp[0]->s_vectorSize);
-    else
-        dsp_add(line_tilde_perf8, 3, x, sp[0]->s_vector, sp[0]->s_vectorSize);
-    x->x_1overn = 1./sp[0]->s_vectorSize;
-    x->x_dspticktomsec = sp[0]->s_sampleRate / (1000 * sp[0]->s_vectorSize);
-}
-
-static void *line_tilde_new(void)
-{
-    t_line_tilde *x = (t_line_tilde *)pd_new(line_tilde_class);
-    outlet_new(&x->x_obj, &s_signal);
-    inlet_newFloat(&x->x_obj, &x->x_inletvalue);
-    x->x_ticksleft = x->x_retarget = 0;
-    x->x_value = x->x_target = x->x_inletvalue = x->x_inletwas = 0;
+    t_line_tilde *x = (t_line_tilde *)pd_new (line_tilde_class);
+    
+    x->x_outlet = outlet_new (cast_object (x), &s_signal);
+    
+    inlet_newFloat (cast_object (x), &x->x_time);
+    
     return x;
 }
 
-void line_tilde_setup(void)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void line_tilde_setup (void)
 {
-    line_tilde_class = class_new(sym_line__tilde__, line_tilde_new, 0,
-        sizeof(t_line_tilde), 0, 0);
-    class_addFloat(line_tilde_class, (t_method)line_tilde_float);
-    class_addMethod(line_tilde_class, (t_method)line_tilde_dsp,
-        sym_dsp, A_CANT, 0);
-    class_addMethod(line_tilde_class, (t_method)line_tilde_stop,
-        sym_stop, 0);
+    t_class *c = NULL;
+    
+    c = class_new (sym_line__tilde__,
+            line_tilde_new,
+            NULL,
+            sizeof (t_line_tilde),
+            CLASS_DEFAULT,
+            A_NULL);
+        
+    class_addDSP (c, line_tilde_dsp);
+    class_addFloat (c, line_tilde_float);
+    
+    class_addMethod (c, (t_method)line_tilde_stop, sym_stop, A_NULL);
+        
+    line_tilde_class = c;
 }
 
 // -----------------------------------------------------------------------------------------------------------
