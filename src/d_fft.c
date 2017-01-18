13b03be374a0de11e955ff22060cda4ce738fbb0
nicolasdanet <nicolas.danet@free.fr>
2017-01-18 10:08:30 +0100

    ...

--- src/d_fft.c
+++ src/d_fft.c
@@ -13,84 +13,92 @@
 #include "m_core.h"
 #include "m_macros.h"
 #include "d_dsp.h"
-#include "d_math.h"
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
-static t_class *sigfft_class;
+static t_class *fft_tilde_class;            /* Shared. */
 
-typedef struct fft
-{
-    t_object x_obj;
-    t_float x_f;
-} t_sigfft;
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-static void *sigfft_new(void)
+typedef struct _fft_tilde {
+    t_object    x_obj;                      /* Must be the first. */
+    t_float     x_f;
+    t_outlet    *x_outletLeft;
+    t_outlet    *x_outletRight;
+    } t_fft_tilde;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+/* No aliasing. */
+
+static t_int *fft_tilde_perform (t_int *w)
 {
-    t_sigfft *x = (t_sigfft *)pd_new(sigfft_class);
-    outlet_new(&x->x_obj, &s_signal);
-    outlet_new(&x->x_obj, &s_signal);
-    inlet_new(&x->x_obj, &x->x_obj.te_g.g_pd, &s_signal, &s_signal);
-    x->x_f = 0;
+    PD_RESTRICTED in1 = (t_sample *)(w[1]);
+    PD_RESTRICTED in2 = (t_sample *)(w[2]);
+    int n = w[3];
+    
+    mayer_fft (n, in1, in2);
+    
+    return (w + 4);
+}
+
+static void fft_tilde_dsp (t_fft_tilde *x, t_signal **sp)
+{
+    int n = sp[0]->s_vectorSize;
+    
+    PD_ASSERT (sp[0]->s_vector != sp[2]->s_vector);
+    PD_ASSERT (sp[1]->s_vector != sp[2]->s_vector);
+    PD_ASSERT (sp[0]->s_vector != sp[3]->s_vector);
+    PD_ASSERT (sp[1]->s_vector != sp[3]->s_vector);
+    PD_ASSERT (sp[2]->s_vector != sp[3]->s_vector);
+    
+    dsp_addCopyPerform (sp[0]->s_vector, sp[2]->s_vector, n);
+    dsp_addCopyPerform (sp[1]->s_vector, sp[3]->s_vector, n);
+    
+    dsp_add (fft_tilde_perform, 3, sp[2]->s_vector, sp[3]->s_vector, n);
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void *fft_tilde_new (void)
+{
+    t_fft_tilde *x = (t_fft_tilde *)pd_new (fft_tilde_class);
+    
+    x->x_outletLeft  = outlet_new (cast_object (x), &s_signal);
+    x->x_outletRight = outlet_new (cast_object (x), &s_signal);
+    
+    inlet_newSignal (cast_object (x));
+
     return x;
 }
 
-static t_int *sigfft_perform(t_int *w)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void fft_tilde_setup (void)
 {
-    t_sample *in1 = (t_sample *)(w[1]);
-    t_sample *in2 = (t_sample *)(w[2]);
-    int n = w[3];
-    mayer_fft(n, in1, in2);
-    return (w+4);
+    t_class *c = NULL;
+    
+    c = class_new (sym_fft__tilde__,
+            fft_tilde_new,
+            NULL,
+            sizeof (t_fft_tilde), 
+            CLASS_DEFAULT,
+            A_NULL);
+            
+    CLASS_SIGNAL (c, t_fft_tilde, x_f);
+    
+    class_addDSP (c, (t_method)fft_tilde_dsp);
+    
+    fft_tilde_class = c;
 }
 
-static t_int *sigfft_swap(t_int *w)
-{
-    t_sample *in1 = (t_sample *)(w[1]);
-    t_sample *in2 = (t_sample *)(w[2]);
-    int n = w[3];
-    for (;n--; in1++, in2++)
-    {   
-        t_sample f = *in1;
-        *in1 = *in2;
-        *in2 = f;
-    }
-    return (w+4);    
-}
-
-static void sigfft_dspx(t_sigfft *x, t_signal **sp, t_int *(*f)(t_int *w))
-{
-    int n = sp[0]->s_vectorSize;
-    t_sample *in1 = sp[0]->s_vector;
-    t_sample *in2 = sp[1]->s_vector;
-    t_sample *out1 = sp[2]->s_vector;
-    t_sample *out2 = sp[3]->s_vector;
-    if (out1 == in2 && out2 == in1)
-        dsp_add(sigfft_swap, 3, out1, out2, n);
-    else if (out1 == in2)
-    {
-        dsp_addCopyPerform (in2, out2, n);
-        dsp_addCopyPerform (in1, out1, n);
-    }
-    else
-    {
-        if (out1 != in1) dsp_addCopyPerform (in1, out1, n);
-        if (out2 != in2) dsp_addCopyPerform (in2, out2, n);
-    }
-    dsp_add(f, 3, sp[2]->s_vector, sp[3]->s_vector, n);
-}
-
-static void sigfft_dsp(t_sigfft *x, t_signal **sp)
-{
-    sigfft_dspx(x, sp, sigfft_perform);
-}
-
-void fft_tilde_setup(void)
-{
-    sigfft_class = class_new(sym_fft__tilde__, sigfft_new, 0,
-        sizeof(t_sigfft), 0, 0);
-    CLASS_SIGNAL(sigfft_class, t_sigfft, x_f);
-    class_addMethod(sigfft_class, (t_method)sigfft_dsp,
-        sym_dsp, A_CANT, 0);
-}
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
