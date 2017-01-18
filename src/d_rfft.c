4257afae740dba6d960421e749da7baac8add223
nicolasdanet <nicolas.danet@free.fr>
2017-01-18 17:28:16 +0100

    ...

--- src/d_rfft.c
+++ src/d_rfft.c
@@ -13,78 +13,114 @@
 #include "m_core.h"
 #include "m_macros.h"
 #include "d_dsp.h"
-#include "d_math.h"
 #include "d_fft.h"
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
+static t_class *rfft_tilde_class;           /* Shared. */
 
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
 
-/* ----------------------- rfft~ -------------------------------- */
+typedef struct _rfft_tilde {
+    t_object    x_obj;                      /* Must be the first. */
+    t_float     x_f;
+    t_outlet    *x_outletLeft;
+    t_outlet    *x_outletRight;
+    } t_rfft_tilde;
 
-static t_class *sigrfft_class;
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
 
-typedef struct rfft
+static t_int *rfft_tilde_performFlipZero (t_int *w)
 {
-    t_object x_obj;
-    t_float x_f;
-} t_sigrfft;
+    PD_RESTRICTED in  = (t_sample *)(w[1]);
+    PD_RESTRICTED out = (t_sample *)(w[2]);
+    int n = w[3];
+    
+    while (n--) { t_sample f = *in; --out; *out = - f; *in = 0.0; in++; }
+        
+    return (w + 4);
+}
 
-static void *sigrfft_new(void)
+static t_int *rfft_tilde_perform (t_int *w)
 {
-    t_sigrfft *x = (t_sigrfft *)pd_new(sigrfft_class);
-    outlet_new(&x->x_obj, &s_signal);
-    outlet_new(&x->x_obj, &s_signal);
-    x->x_f = 0;
+    PD_RESTRICTED in = (t_sample *)(w[1]);
+    int n = w[2];
+    
+    mayer_realfft (n, in);
+    
+    return (w + 3);
+}
+
+static void rfft_tilde_dsp (t_rfft_tilde *x, t_signal **sp)
+{
+    int size = sp[0]->s_vectorSize;
+    
+    PD_ASSERT (PD_IS_POWER_2 (size));
+    PD_ASSERT (sp[0]->s_vector != sp[1]->s_vector);
+    PD_ASSERT (sp[0]->s_vector != sp[2]->s_vector);
+    PD_ASSERT (sp[1]->s_vector != sp[2]->s_vector);
+    
+    if (size < 4) { error_invalid (sym_rfft__tilde__, sym_size); }
+    else {
+    //
+    PD_RESTRICTED in1  = sp[0]->s_vector;
+    PD_RESTRICTED out1 = sp[1]->s_vector;
+    PD_RESTRICTED out2 = sp[2]->s_vector;
+    
+    int half = (size >> 1);
+    
+    dsp_addCopyPerform (in1, out1, size);
+    
+    dsp_add (rfft_tilde_perform, 2, out1, size);
+    dsp_add (rfft_tilde_performFlipZero, 3, out1 + half + 1, out2 + half, half - 1);
+    
+    dsp_addZeroPerform (out2 + half, half);
+    dsp_addZeroPerform (out2, 1);
+    //
+    }
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static void *rfft_tilde_new (void)
+{
+    t_rfft_tilde *x = (t_rfft_tilde *)pd_new (rfft_tilde_class);
+    
+    x->x_outletLeft  = outlet_new (cast_object (x), &s_signal);
+    x->x_outletRight = outlet_new (cast_object (x), &s_signal);
+
     return x;
 }
 
-static t_int *sigrfft_perform(t_int *w)
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void rfft_tilde_setup (void)
 {
-    t_sample *in = (t_sample *)(w[1]);
-    int n = w[2];
-    mayer_realfft(n, in);
-    return (w+3);
+    t_class *c = NULL;
+    
+    c = class_new (sym_rfft__tilde__,
+            rfft_tilde_new,
+            NULL,
+            sizeof (t_rfft_tilde),
+            CLASS_DEFAULT,
+            A_NULL);
+            
+    CLASS_SIGNAL (c, t_rfft_tilde, x_f);
+    
+    class_addDSP (c, (t_method)rfft_tilde_dsp);
+    
+    class_setHelpName (c, sym_fft__tilde__);
+    
+    rfft_tilde_class = c;
 }
 
-static t_int *sigrfft_flip(t_int *w)
-{
-    t_sample *in = (t_sample *)(w[1]);
-    t_sample *out = (t_sample *)(w[2]);
-    int n = w[3];
-    while (n--)
-        *(--out) = - *in++;
-    return (w+4);
-}
-
-static void sigrfft_dsp(t_sigrfft *x, t_signal **sp)
-{
-    int n = sp[0]->s_vectorSize, n2 = (n>>1);
-    t_sample *in1 = sp[0]->s_vector;
-    t_sample *out1 = sp[1]->s_vector;
-    t_sample *out2 = sp[2]->s_vector;
-    if (n < 4)
-    {
-        post_error ("fft: minimum 4 points");
-        return;
-    }
-    if (in1 != out1)
-        dsp_addCopyPerform (in1, out1, n);
-    dsp_add(sigrfft_perform, 2, out1, n);
-    dsp_add(sigrfft_flip, 3, out1 + (n2+1), out2 + n2, n2-1);
-    dsp_addZeroPerform(out1 + (n2+1), ((n2-1)&(~7)));
-    dsp_addZeroPerform(out1 + (n2+1) + ((n2-1)&(~7)), ((n2-1)&7));
-    dsp_addZeroPerform(out2 + n2, n2);
-    dsp_addZeroPerform(out2, 1);
-}
-
-void rfft_tilde_setup(void)
-{
-    sigrfft_class = class_new(sym_rfft__tilde__, sigrfft_new, 0,
-        sizeof(t_sigrfft), 0, 0);
-    CLASS_SIGNAL(sigrfft_class, t_sigrfft, x_f);
-    class_addMethod(sigrfft_class, (t_method)sigrfft_dsp,
-        sym_dsp, A_CANT, 0);
-    class_setHelpName(sigrfft_class, sym_fft__tilde__);
-}
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
