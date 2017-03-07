cf2611d2e9817d347f67493d1ac2cafd3ec3112a
nicolasdanet <nicolas.danet@free.fr>
2017-03-07 09:33:47 +0100

    ...

--- src/m_core.h
+++ src/m_core.h
@@ -21,27 +21,6 @@
 // -----------------------------------------------------------------------------------------------------------
 #pragma mark -
 
-typedef void (*t_freemethod)            (t_pd *x);
-typedef void (*t_bangmethod)            (t_pd *x);
-typedef void (*t_floatmethod)           (t_pd *x, t_float f);
-typedef void (*t_symbolmethod)          (t_pd *x, t_symbol *s);
-typedef void (*t_listmethod)            (t_pd *x, t_symbol *s, int argc, t_atom *argv);
-typedef void (*t_anythingmethod)        (t_pd *x, t_symbol *s, int argc, t_atom *argv);
-typedef void (*t_pointermethod)         (t_pd *x, t_gpointer *gp);
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-#pragma mark -
-
-typedef void (*t_savefn)                (t_gobj *x, t_buffer *b);
-typedef void (*t_propertiesfn)          (t_gobj *x, t_glist *glist);
-typedef void (*t_drawfn)                (t_gobj *x, t_glist *glist);
-typedef void (*t_motionfn)              (void *z, t_float deltaX, t_float deltaY, t_float modifier);
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-#pragma mark -
-
 typedef struct _mouse {
     int         m_x;
     int         m_y;
@@ -70,93 +49,17 @@ typedef struct _bounds {
     
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
-#pragma mark -
-
-typedef void (*t_getrectanglefn)        (t_gobj *x, t_glist *glist, t_rectangle *r);
-typedef void (*t_displacedfn)           (t_gobj *x, t_glist *glist, int deltaX, int deltaY);
-typedef void (*t_selectedfn)            (t_gobj *x, t_glist *glist, int isSelected);
-typedef void (*t_activatedfn)           (t_gobj *x, t_glist *glist, int isActivated);
-typedef void (*t_deletedfn)             (t_gobj *x, t_glist *glist);
-typedef void (*t_visibilityfn)          (t_gobj *x, t_glist *glist, int isVisible);
-typedef int  (*t_mousefn)               (t_gobj *x, t_glist *glist, t_mouse *m);
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-#pragma mark -
-
-typedef void (*t_paintergetrectanglefn) (t_gobj *x, t_gpointer *gp, t_float a, t_float b, t_rectangle *r);
-typedef void (*t_paintervisibilityfn)   (t_gobj *x, t_gpointer *gp, t_float a, t_float b, int flag);
-typedef int  (*t_paintermousefn)        (t_gobj *x, t_gpointer *gp, t_float a, t_float b, t_mouse *m);
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-
-struct _widgetbehavior {
-    t_getrectanglefn            w_fnGetRectangle;
-    t_displacedfn               w_fnDisplaced;
-    t_selectedfn                w_fnSelected;
-    t_activatedfn               w_fnActivated;
-    t_deletedfn                 w_fnDeleted;
-    t_visibilityfn              w_fnVisibilityChanged;
-    t_mousefn                   w_fnMouse;
-    };
-    
-struct _painterwidgetbehavior {
-    t_paintergetrectanglefn     w_fnPainterGetRectangle;
-    t_paintervisibilityfn       w_fnPainterVisibilityChanged;
-    t_paintermousefn            w_fnPainterMouse;
-    };
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-#pragma mark -
-
-typedef struct _entry {
-    t_symbol                    *me_name;
-    t_method                    me_method;
-    t_atomtype                  me_arguments[PD_ARGUMENTS + 1];
-    } t_entry;
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-
-struct _class {
-    t_symbol                    *c_name;
-    t_symbol                    *c_helpName;
-    t_symbol                    *c_externalDirectory;
-    t_entry                     *c_methods;
-    int                         c_methodsSize;
-    t_freemethod                c_methodFree;
-    t_bangmethod                c_methodBang;
-    t_floatmethod               c_methodFloat;
-    t_symbolmethod              c_methodSymbol;
-    t_listmethod                c_methodList;
-    t_anythingmethod            c_methodAnything;
-    t_pointermethod             c_methodPointer;
-    t_widgetbehavior            *c_behavior;
-    t_painterwidgetbehavior     *c_behaviorPainter;
-    t_savefn                    c_fnSave;
-    t_propertiesfn              c_fnProperties;
-    int                         c_signalOffset;
-    char                        c_isBox;
-    char                        c_hasFirstInlet;
-    int                         c_type;
-    size_t                      c_size;
-    };
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
 
 struct _pdinstance {
-    t_systime                   pd_systime;
-    int                         pd_dspState;
-    int                         pd_dspChainSize;
-    t_int                       *pd_dspChain;
-    t_clock                     *pd_clocks;
-    t_signal                    *pd_signals;
-    t_glist                     *pd_roots;
-    t_clock                     *pd_polling;
-    t_clock                     *pd_autorelease;
+    t_systime   pd_systime;
+    int         pd_dspState;
+    int         pd_dspChainSize;
+    t_int       *pd_dspChain;
+    t_clock     *pd_clocks;
+    t_signal    *pd_signals;
+    t_glist     *pd_roots;
+    t_clock     *pd_polling;
+    t_clock     *pd_autorelease;
     };
 
 // -----------------------------------------------------------------------------------------------------------
@@ -231,35 +134,6 @@ void        instance_freeAllRoots                       (void);
 
 void        message_initialize                          (void);
 void        message_release                             (void);
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-#pragma mark -
-
-void        class_setCurrentExternalDirectory           (t_symbol *s);
-
-t_method    class_getMethod                             (t_class *c, t_symbol *s);
-int         class_hasMethod                             (t_class *c, t_symbol *s);
-int         class_hasDSP                                (t_class *c);
-int         class_hasBang                               (t_class *c);
-int         class_hasDrawCommand                        (t_class *c);
-int         class_hasPropertiesFunction                 (t_class *c); 
-void        class_setWidgetBehavior                     (t_class *c, t_widgetbehavior *w);
-void        class_setPainterWidgetBehavior              (t_class *c, t_painterwidgetbehavior *w);
-void        class_setHelpName                           (t_class *c, t_symbol *s);
-void        class_setPropertiesFunction                 (t_class *c, t_propertiesfn f);
-void        class_setSaveFunction                       (t_class *c, t_savefn f);
-
-t_symbol    *class_getName                              (t_class *c);
-char        *class_getNameAsString                      (t_class *c);
-char        *class_getHelpNameAsString                  (t_class *c);
-char        *class_getExternalDirectoryAsString         (t_class *c);
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-
-t_painterwidgetbehavior *class_getPainterWidget         (t_class *c);
-t_propertiesfn          class_getPropertiesFunction     (t_class *c);
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
@@ -408,12 +282,13 @@ void        guistub_destroyWithKey                      (void *key);
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
-#include "h_helpers.h"
+#include "m_class.h"
 #include "m_object.h"
 #include "m_error.h"
 #include "m_utils.h"
 #include "m_rectangle.h"
 #include "m_symbols.h"
+#include "h_helpers.h"
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
