93f7aaeb6d0f3fa18b82febffa70e9430f3dbcb3
nicolasdanet <nicolas.danet@free.fr>
2017-03-23 10:06:00 +0100

    ...

---
+++ src/m_extern.h
@@ -0,0 +1,39 @@
+
+/* 
+    Copyright (c) 1997-2016 Miller Puckette and others.
+*/
+
+/* < https://opensource.org/licenses/BSD-3-Clause > */
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+#ifndef __m_extern_h_
+#define __m_extern_h_
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+extern t_class *block_class;
+extern t_class *canvas_class;
+extern t_class *catch_tilde_class;
+extern t_class *delwrite_tilde_class;
+extern t_class *floatinlet_class;
+extern t_class *garray_class;
+extern t_class *inlet_class;
+extern t_class *panel_class;
+extern t_class *pointerinlet_class;
+extern t_class *scalar_class;
+extern t_class *send_tilde_class;
+extern t_class *struct_class;
+extern t_class *symbolinlet_class;
+extern t_class *text_class;
+extern t_class *textdefine_class;
+extern t_class *vinlet_class;
+extern t_class *voutlet_class;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+#endif // __m_extern_h_
