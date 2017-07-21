6c02fd3f86fcf9decd9648b24c8981a3c328974d
nicolasdanet <nicolas.danet@free.fr>
2017-07-21 14:42:40 +0200

    ...

--- src/x_arrayrandom.c
+++ src/x_arrayrandom.c
@@ -5,11 +5,6 @@
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
-
-/* Homebrew PRNG kept for compatibility. */
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
 #include "m_pd.h"
@@ -27,7 +22,7 @@ static t_class *arrayrandom_class;          /* Shared. */
 
 typedef struct _arrayrandom {
     t_arrayrange    x_arrayrange;           /* Must be the first. */
-    unsigned int    x_state;
+    t_seed          x_state;
     t_outlet        *x_outlet;
     } t_arrayrandom;
 
@@ -39,9 +34,7 @@ static void arrayrandom_bang (t_arrayrandom *x)
 {
     if (!arrayrange_isValid (&x->x_arrayrange)) { error_invalid (sym_array__space__random, sym_field); }
     else {
-        double k;
-        x->x_state = x->x_state * 472940017 + 832416023;
-        k = x->x_state * (1.0 / 4294967296.0);
+        double k = PD_RAND48_DOUBLE (x->x_state);
         outlet_float (x->x_outlet, arrayrange_quantile (&x->x_arrayrange, (t_float)k));
     }
 }
@@ -53,7 +46,7 @@ static void arrayrandom_float (t_arrayrandom *x, t_float f)
 
 static void arrayrandom_seed (t_arrayrandom *x, t_float f)
 {
-    x->x_state = f;
+    x->x_state = (t_seed)f;
 }
 
 // -----------------------------------------------------------------------------------------------------------
@@ -62,14 +55,10 @@ static void arrayrandom_seed (t_arrayrandom *x, t_float f)
 
 void *arrayrandom_new (t_symbol *s, int argc, t_atom *argv)
 {
-    static unsigned int random_nextSeed = 584926371;
-    
     t_arrayrandom *x = (t_arrayrandom *)arrayrange_new (arrayrandom_class, argc, argv, 0, 1);
     
-    random_nextSeed = random_nextSeed * 435898247 + 938284287;
-
     if (x) {
-        x->x_state  = random_nextSeed;
+        PD_RAND48_INIT (x->x_state);
         x->x_outlet = outlet_new (cast_object (x), &s_float);
         
     } else {
