2c046a5fdfe9776eb8c5a77fc3945cd1616c21f0
nicolasdanet <nicolas.danet@free.fr>
2017-08-20 15:21:38 +0200

    ...

--- src/x_atan2.c
+++ src/x_atan2.c
@@ -20,7 +20,8 @@ static t_class *atan2_class;        /* Shared. */
 
 typedef struct _atan2 {
     t_object    x_obj;
-    t_float     x_f;
+    t_float     x_f1;
+    t_float     x_f2;
     t_outlet    *x_outlet;
     } t_atan2;
 
@@ -32,17 +33,21 @@ static void *atan2_new (void)
 {
     t_atan2 *x = (t_atan2 *)pd_new (atan2_class);
     
-    x->x_f = (t_float)0.0;
     x->x_outlet = outlet_new (cast_object (x), &s_float);
         
-    inlet_newFloat (cast_object (x), &x->x_f);
+    inlet_newFloat (cast_object (x), &x->x_f2);
 
     return x;
 }
 
+static void atan2_bang (t_atan2 *x)
+{
+    outlet_float (x->x_outlet, (x->x_f1 == 0.0 && x->x_f2 == 0.0 ? (t_float)0.0 : atan2f (x->x_f1, x->x_f2)));
+}
+
 static void atan2_float (t_atan2 *x, t_float f)
 {
-    outlet_float (x->x_outlet, (f == 0.0 && x->x_f == 0.0 ? (t_float)0.0 : atan2f (f, x->x_f)));
+    x->x_f1 = f; atan2_bang (x);
 }
 
 // -----------------------------------------------------------------------------------------------------------
@@ -59,7 +64,8 @@ void atan2_setup (void)
             sizeof (t_atan2),
             CLASS_DEFAULT,
             A_NULL);
-            
+    
+    class_addBang (c, (t_method)atan2_bang);
     class_addFloat (c, (t_method)atan2_float);
     
     class_setHelpName (c, sym_sqrt);
