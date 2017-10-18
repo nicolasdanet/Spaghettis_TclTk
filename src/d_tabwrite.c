97f149d95c71daed0e3177db1ae5b8950f0f8d95
nicolasdanet <nicolas.danet@free.fr>
2017-10-18 09:39:06 +0200

    ...

--- src/d_tabwrite.c
+++ src/d_tabwrite.c
@@ -27,8 +27,10 @@ typedef struct _tabwrite_tilde {
     int         x_redraw;
     int         x_phase;
     int         x_size;
+    int         x_period;
     t_word      *x_vector;
     t_symbol    *x_name;
+    t_clock     *x_clock;
     } t_tabwrite_tilde;
 
 // -----------------------------------------------------------------------------------------------------------
@@ -58,6 +60,11 @@ void tab_fetchArray (t_symbol *s, int *size, t_word **data, t_symbol *err)
 static void tabwrite_tilde_bang (t_tabwrite_tilde *x)
 {
     x->x_phase = 0;
+}
+
+static void tabwrite_tilde_task (t_tabwrite_tilde *x)
+{
+    tabwrite_tilde_bang (x); clock_delay (x->x_clock, x->x_period);
 }
 
 static void tabwrite_tilde_polling (t_tabwrite_tilde *x)
@@ -145,12 +152,16 @@ static void tabwrite_tilde_dsp (t_tabwrite_tilde *x, t_signal **sp)
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-static void *tabwrite_tilde_new (t_symbol *s)
+static void *tabwrite_tilde_new (t_symbol *s, t_float f)
 {
     t_tabwrite_tilde *x = (t_tabwrite_tilde *)pd_new (tabwrite_tilde_class);
     
-    x->x_phase = PD_INT_MAX;
-    x->x_name  = s;
+    x->x_phase  = PD_INT_MAX;
+    x->x_period = PD_MAX (0.0, f);
+    x->x_name   = s;
+    x->x_clock  = clock_new ((void *)x, (t_method)tabwrite_tilde_task);
+    
+    if (x->x_period > 0.0) { clock_delay (x->x_clock, x->x_period); }
     
     instance_pollingRegister (cast_pd (x));
     
@@ -159,6 +170,8 @@ static void *tabwrite_tilde_new (t_symbol *s)
 
 static void tabwrite_tilde_free (t_tabwrite_tilde *x)
 {
+    clock_free (x->x_clock);
+    
     instance_pollingUnregister (cast_pd (x));
 }
 
@@ -176,6 +189,7 @@ void tabwrite_tilde_setup (void)
             sizeof (t_tabwrite_tilde),
             CLASS_DEFAULT,
             A_DEFSYMBOL,
+            A_DEFFLOAT,
             A_NULL);
             
     CLASS_SIGNAL (c, t_tabwrite_tilde, x_f);
