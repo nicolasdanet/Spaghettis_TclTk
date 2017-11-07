3d2b383be0eed0f74ed2b15da2d9f4f3c1e68975
nicolasdanet <nicolas.danet@free.fr>
2017-11-07 09:32:23 +0100

    ...

--- src/x_title.c
+++ src/x_title.c
@@ -22,6 +22,7 @@ static t_class *title_class;            /* Shared. */
 typedef struct _title {
     t_object    x_obj;                  /* Must be the first. */
     t_glist     *x_owner;
+    t_symbol    *x_name;
     t_outlet    *x_outlet;
     } t_title;
 
@@ -29,9 +30,25 @@ typedef struct _title {
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
+t_symbol *title_getName (t_title *x)
+{
+    if (glist_isTop (x->x_owner)) { return symbol_removeExtension (x->x_name); }
+    
+    return x->x_name;
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
 static void title_bang (t_title *x)
 {
+    outlet_symbol (x->x_outlet, title_getName (x));
+}
 
+static void title_polling (t_title *x)
+{
+    t_symbol *t = glist_getName (x->x_owner); if (x->x_name != t) { x->x_name = t; title_bang (x); }
 }
 
 // -----------------------------------------------------------------------------------------------------------
@@ -43,9 +60,17 @@ static void *title_new (void)
     t_title *x = (t_title *)pd_new (title_class);
     
     x->x_owner  = instance_contextGetCurrent();
+    x->x_name   = glist_getName (x->x_owner);
     x->x_outlet = outlet_new (cast_object (x), &s_symbol);
     
+    instance_pollingRegister (cast_pd (x));
+    
     return x;
+}
+
+static void title_free (t_title *x)
+{
+    instance_pollingUnregister (cast_pd (x));
 }
 
 // -----------------------------------------------------------------------------------------------------------
@@ -58,12 +83,13 @@ void title_setup (void)
     
     c = class_new (sym_title,
             (t_newmethod)title_new,
-            NULL,
+            (t_method)title_free,
             sizeof (t_title),
             CLASS_DEFAULT,
             A_NULL);
 
     class_addBang (c, (t_method)title_bang);
+    class_addPolling (c, (t_method)title_polling);
     
     title_class = c;
 }
