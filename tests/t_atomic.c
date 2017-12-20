3611626e30e1ff57bc2997e7e2d21c76abfbcf4e
nicolasdanet <nicolas.danet@free.fr>
2017-12-20 07:37:56 +0100

    ...

--- tests/t_atomic.c
+++ tests/t_atomic.c
@@ -1,6 +1,4 @@
 
-/* Copyright (c) 1997-2017 Miller Puckette and others. */
-
 /* < https://opensource.org/licenses/BSD-3-Clause > */
 
 // -----------------------------------------------------------------------------------------------------------
