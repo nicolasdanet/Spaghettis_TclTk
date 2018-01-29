8d91c1b38f9e7a4b339d7ff560f686f53cab168c
nicolasdanet <nicolas.danet@free.fr>
2018-01-29 14:20:24 +0100

    ...

--- src/d_block.h
+++ src/d_block.h
@@ -7,42 +7,55 @@
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-#ifndef __d_resample_h_
-#define __d_resample_h_
+#ifndef __d_block_h_
+#define __d_block_h_
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-typedef struct _resample {
-    int         r_type;
-    int         r_downsample;
-    int         r_upsample;
-    t_sample    r_buffer;
-    int         r_allocatedSize;
-    t_sample    *r_vector;
-    } t_resample;
+typedef struct _blockproperties {
+    int         bp_switchable;
+    int         bp_reblocked;
+    int         bp_blockSize;
+    t_float     bp_sampleRate;
+    int         bp_period;
+    int         bp_frequency;
+    int         bp_downsample;
+    int         bp_upsample;
+    } t_blockproperties;
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+typedef struct _block {
+    t_object    bk_obj;                 /* Must be the first. */
+    int         bk_blockSize;
+    int         bk_overlap;
+    int         bk_phase;
+    int         bk_period;
+    int         bk_frequency;
+    int         bk_downsample;
+    int         bk_upsample;
+    int         bk_isSwitchObject;
+    int         bk_isSwitchedOn;
+    int         bk_isReblocked;
+    int         bk_allContextLength;
+    int         bk_outletEpilogLength;
+    int         bk_count;
+    } t_block;
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
-void        resample_init           (t_resample *x, t_symbol *type);
-void        resample_free           (t_resample *x);
-void        resample_setRatio       (t_resample *x, int downsample, int upsample);
-int         resample_isRequired     (t_resample *x);
-void        resample_toDsp          (t_resample *x, t_sample *s, int vectorSize, int resampledSize);
-t_sample    *resample_fromDsp       (t_resample *x, t_sample *s, int vectorSize, int resampledSize);
+t_int       *block_performProlog        (t_int *w);
+t_int       *block_performEpilog        (t_int *w);
+
+t_float     block_getRatio              (t_block *x);
+void        block_getProperties         (t_block *x, t_blockproperties *properties);
+void        block_setPerformsLength     (t_block *x, int allContextLength, int epilogLength);
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
-// MARK: -
-
-static inline t_sample *resample_vector (t_resample *x)
-{
-    return x->r_vector;
-}
-
-// -----------------------------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------------------------
-#endif // __d_resample_h_
+#endif // __d_block_h_
