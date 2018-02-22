44e95f4daaae6dc9d1efde4bb4db40e08dbb6830
nicolasdanet <nicolas.danet@free.fr>
2018-02-22 11:19:51 +0100

    ...

--- src/g_slider.c
+++ src/g_slider.c
@@ -534,30 +534,30 @@ static void slider_functionSave (t_gobj *z, t_buffer *b)
 
     iemgui_serialize (cast_iem (z), &names, &colors);
     
-    buffer_vAppend (b, "ssiisiiffiisssiiiisssii;", 
-        sym___hash__X,
-        sym_obj,
-        object_getX (cast_object (z)), 
-        object_getY (cast_object (z)),
-        x->x_isVertical ? sym_vslider : sym_hslider, 
-        x->x_gui.iem_width,
-        x->x_gui.iem_height,
-        (t_float)x->x_minimum,
-        (t_float)x->x_maximum,
-        x->x_isLogarithmic,
-        iemgui_serializeLoadbang (cast_iem (z)),
-        names.n_unexpandedSend,
-        names.n_unexpandedReceive,
-        names.n_unexpandedLabel,
-        x->x_gui.iem_labelX,
-        x->x_gui.iem_labelY,
-        iemgui_serializeFontStyle (cast_iem (z)),
-        x->x_gui.iem_fontSize,
-        colors.c_symColorBackground,
-        colors.c_symColorForeground,
-        colors.c_symColorLabel,
-        x->x_position,
-        x->x_isSteadyOnClick);
+    buffer_appendSymbol (b, sym___hash__X);
+    buffer_appendSymbol (b, sym_obj);
+    buffer_appendFloat (b,  object_getX (cast_object (z)));
+    buffer_appendFloat (b,  object_getY (cast_object (z)));
+    buffer_appendSymbol (b, x->x_isVertical ? sym_vslider : sym_hslider);
+    buffer_appendFloat (b,  x->x_gui.iem_width);
+    buffer_appendFloat (b,  x->x_gui.iem_height);
+    buffer_appendFloat (b,  (t_float)x->x_minimum);
+    buffer_appendFloat (b,  (t_float)x->x_maximum);
+    buffer_appendFloat (b,  x->x_isLogarithmic);
+    buffer_appendFloat (b,  iemgui_serializeLoadbang (cast_iem (z)));
+    buffer_appendSymbol (b, names.n_unexpandedSend);
+    buffer_appendSymbol (b, names.n_unexpandedReceive);
+    buffer_appendSymbol (b, names.n_unexpandedLabel);
+    buffer_appendFloat (b,  x->x_gui.iem_labelX);
+    buffer_appendFloat (b,  x->x_gui.iem_labelY);
+    buffer_appendFloat (b,  iemgui_serializeFontStyle (cast_iem (z)));
+    buffer_appendFloat (b,  x->x_gui.iem_fontSize);
+    buffer_appendSymbol (b, colors.c_symColorBackground);
+    buffer_appendSymbol (b, colors.c_symColorForeground);
+    buffer_appendSymbol (b, colors.c_symColorLabel);
+    buffer_appendFloat (b,  x->x_position);
+    buffer_appendFloat (b,  x->x_isSteadyOnClick);
+    buffer_appendSemicolon (b);
 }
 
 static void slider_functionProperties (t_gobj *z, t_glist *owner)
