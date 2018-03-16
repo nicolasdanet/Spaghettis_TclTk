cddf289275fe43f2efd7d3012430e3467f9b918c
nicolasdanet <nicolas.danet@free.fr>
2018-03-16 16:50:57 +0100

    ...

--- src/x_mica.c
+++ src/x_mica.c
@@ -146,13 +146,13 @@ static void *mica_makeObject (t_symbol *s, int argc, t_atom *argv)
     t_symbol *t = atom_getSymbolAtIndex (0, argc, argv);
     
     if (t == sym_set)               { newest = (t_pd *)micaset_new (s, argc - 1, argv + 1); }
-    else if (t == sym_get)          { }
-    else if (t == sym_map)          { }
-    else if (t == sym_index)        { }
-    else if (t == sym_item)         { }
-    else if (t == sym_info)         { }
-    else if (t == sym_interval)     { }
-    else if (t == sym_spell)        { }
+    //else if (t == sym_get)          { }
+    //else if (t == sym_map)          { }
+    //else if (t == sym_index)        { }
+    //else if (t == sym_item)         { }
+    //else if (t == sym_info)         { }
+    //else if (t == sym_interval)     { }
+    //else if (t == sym_spell)        { }
     else {
         newest = (t_pd *)micaset_new (s, argc, argv);
     }
