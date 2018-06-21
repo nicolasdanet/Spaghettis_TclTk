f144a354f787cec9e6bda2052d3b658c6ced657a
nicolasdanet <nicolas.danet@free.fr>
2018-06-21 10:17:51 +0200

    Allow opposite in field name for coordinates

--- src/graphics/scalars/g_field.c
+++ src/graphics/scalars/g_field.c
@@ -19,18 +19,73 @@
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 
+static int field_isVariableOpposite (t_symbol *s)
+{
+    if (string_startWith (s->s_name, "(-")) {
+    if (string_endWith (s->s_name, ")")) {
+        return 1;
+    }
+    }
+    
+    return 0;
+}
+
+static t_symbol *field_getVariableNameParseOpposite (t_symbol *s)
+{
+    if (field_isVariableOpposite (s)) {
+    //
+    char t[PD_STRING] = { 0 };
+    t_error err = string_copy (t, PD_STRING, s->s_name);
+    PD_ASSERT (!err);
+    if (!err) {
+        size_t n = strlen (t);
+        t[n - 1] = 0;
+        return gensym (t + 2);
+    }
+    //
+    }
+    
+    return s;
+}
+
+static void field_setAsFloatVariableParseOpposite (t_fielddescriptor *fd, t_symbol *s)
+{
+    fd->fd_type               = DATA_FLOAT;
+    fd->fd_isVariable         = 1;
+    fd->fd_isVariableOpposite = field_isVariableOpposite (s);
+    fd->fd_un.fd_variableName = field_getVariableNameParseOpposite (s);
+}
+
+void field_setAsFloatParseOpposite (t_fielddescriptor *fd, int argc, t_atom *argv)
+{
+    field_setAsFloatConstant (fd, 0.0);
+    
+    if (argc > 0) {
+        if (IS_SYMBOL (argv)) { field_setAsFloatVariableParseOpposite (fd, GET_SYMBOL (argv)); }
+        else {
+            field_setAsFloatConstant (fd, atom_getFloatAtIndex (0, argc, argv));
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+// MARK: -
+
 void field_setAsFloatConstant (t_fielddescriptor *fd, t_float f)
 {
-    fd->fd_type                 = DATA_FLOAT;
-    fd->fd_isVariable           = 0;
-    fd->fd_un.fd_float          = f;
+    fd->fd_type               = DATA_FLOAT;
+    fd->fd_isVariable         = 0;
+    fd->fd_isVariableOpposite = 0;
+    fd->fd_un.fd_float        = f;
 }
 
 void field_setAsFloatVariable (t_fielddescriptor *fd, t_symbol *s)
 {
-    fd->fd_type                 = DATA_FLOAT;
-    fd->fd_isVariable           = 1;
-    fd->fd_un.fd_variableName   = s;
+    fd->fd_type               = DATA_FLOAT;
+    fd->fd_isVariable         = 1;
+    fd->fd_isVariableOpposite = 0;
+    fd->fd_un.fd_variableName = s;
 }
 
 void field_setAsFloat (t_fielddescriptor *fd, int argc, t_atom *argv)
@@ -49,12 +104,13 @@ void field_setAsArray (t_fielddescriptor *fd, int argc, t_atom *argv)
 {
     field_setAsFloatConstant (fd, 0.0);
     
-    if (argc > 0) {
-        if (IS_SYMBOL (argv)) {
-            fd->fd_type                 = DATA_ARRAY;
-            fd->fd_isVariable           = 1;
-            fd->fd_un.fd_variableName   = GET_SYMBOL (argv);
-        }
+    if (argc > 0 && IS_SYMBOL (argv)) {
+    //
+    fd->fd_type               = DATA_ARRAY;
+    fd->fd_isVariable         = 1;
+    fd->fd_isVariableOpposite = 0;
+    fd->fd_un.fd_variableName = GET_SYMBOL (argv);
+    //
     }
 }
 
