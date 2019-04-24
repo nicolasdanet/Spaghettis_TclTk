96d215debf3ac3ba41a8cb5558b12ec72bd7dfed
nicolasdanet <nicolas.danet@free.fr>
2019-04-24 15:54:07 +0200

    Fix cos~ signals

--- src/dsp/osc/d_cos.c
+++ src/dsp/osc/d_cos.c
@@ -112,6 +112,8 @@ static void cos_tilde_dsp (t_cos_tilde *x, t_signal **sp)
 {
     PD_ASSERT (sp[0]->s_vector != sp[1]->s_vector);
     
+    object_fetchAndCopySignalValuesIfRequired (cast_object (x));
+
     dsp_add (cos_tilde_perform, 3, sp[0]->s_vector, sp[1]->s_vector, sp[0]->s_vectorSize);
 }
 
