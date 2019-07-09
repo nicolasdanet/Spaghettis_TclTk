e37eb93e86d334c2a78cbd4a8cdf4ba3f1c3e7cb
nicolasdanet <nicolas.danet@free.fr>
2019-07-09 08:47:49 +0200

    Change value object to use lists

--- src/control/flow/x_value.c
+++ src/control/flow/x_value.c
@@ -9,6 +9,13 @@
 
 #include "../../m_spaghettis.h"
 #include "../../m_core.h"
+#include "../../s_system.h"
+#include "../../g_graphics.h"
+
+// -----------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------
+
+#include "../list/x_list.h"
 
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
@@ -20,16 +27,16 @@ static t_class *value_class;                /* Shared. */
 // -----------------------------------------------------------------------------------------------------------
 
 typedef struct _valuecommon {
-    t_pd        x_pd;                       /* Must be the first. */
-    int         x_referenceCount;
-    t_float     x_f;
+    t_pd            x_pd;                   /* Must be the first. */
+    int             x_referenceCount;
+    t_listinlet     x_list;
     } t_valuecommon;
 
 typedef struct _value {
-    t_object    x_obj;                      /* Must be the first. */
-    t_symbol    *x_name;
-    t_float     *x_raw;
-    t_outlet    *x_outlet;
+    t_object        x_obj;                  /* Must be the first. */
+    t_symbol        *x_name;
+    t_listinlet     *x_raw;
+    t_outlet        *x_outlet;
     } t_value;
 
 // -----------------------------------------------------------------------------------------------------------
@@ -41,7 +48,7 @@ static t_symbol *valuecommon_name (t_symbol *s)
     return symbol_addPrefix (s, sym__VALUE_);
 }
 
-static t_float *valuecommon_fetch (t_symbol *s)
+static t_listinlet *valuecommon_fetch (t_symbol *s)
 {
     t_symbol *name   = valuecommon_name (s);
     t_valuecommon *x = (t_valuecommon *)symbol_getThingByClass (name, valuecommon_class);
@@ -49,13 +56,14 @@ static t_float *valuecommon_fetch (t_symbol *s)
     if (!x) {
         x = (t_valuecommon *)pd_new (valuecommon_class);
         x->x_referenceCount = 0;
-        x->x_f = 0;
+        listinlet_init (&x->x_list);
+            t_atom a; SET_FLOAT (&a, 0.0); listinlet_listSet (&x->x_list, 1, &a);
         pd_bind (cast_pd (x), name);
     }
     
     x->x_referenceCount++;
     
-    return (&x->x_f);
+    return (&x->x_list);
 }
 
 static void valuecommon_release (t_symbol *s)
@@ -67,7 +75,11 @@ static void valuecommon_release (t_symbol *s)
     
     x->x_referenceCount--;
     
-    if (!x->x_referenceCount) { pd_unbind (cast_pd (x), name); pd_free (cast_pd (x)); }
+    if (!x->x_referenceCount) {
+    //
+    listinlet_free (&x->x_list); pd_unbind (cast_pd (x), name); pd_free (cast_pd (x));
+    //
+    }
 }
 
 static int valuecommon_count (t_symbol *s)
@@ -94,18 +106,51 @@ static void value_set (t_value *x, t_symbol *s)
     if (x->x_name != &s_) { x->x_raw = valuecommon_fetch (x->x_name); }
 }
 
+static void value_output (t_value *x)
+{
+    t_atom *t = NULL;
+    int count = listinlet_listSize (x->x_raw);
+    
+    if (!count) { outlet_list (x->x_outlet, 0, NULL); }
+    else {
+    //
+    PD_ATOMS_ALLOCA (t, count);
+
+    if (listinlet_listHasPointer (x->x_raw)) {
+    
+        t_listinlet cache; listinlet_initByClone (x->x_raw, &cache);
+        listinlet_copyAtomsUnchecked (&cache, t);
+        outlet_list (x->x_outlet, count, t);
+        listinlet_free (&cache);
+        
+    } else {
+    
+        listinlet_copyAtomsUnchecked (x->x_raw, t);
+        outlet_list (x->x_outlet, count, t);
+    }
+
+    PD_ATOMS_FREEA (t, count);
+    //
+    }
+}
+
 // -----------------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 // MARK: -
 
 static void value_bang (t_value *x)
 {
-    if (x->x_raw) { outlet_float (x->x_outlet, *x->x_raw); }
+    if (x->x_raw) { value_output (x); }
 }
 
-static void value_float (t_value *x, t_float f)
+static void value_list (t_value *x, t_symbol *s, int argc, t_atom *argv)
 {
-    if (x->x_raw) { *x->x_raw = f; }
+    if (x->x_raw) { listinlet_listSet (x->x_raw, argc, argv); }
+}
+
+static void value_anything (t_value *x, t_symbol *s, int argc, t_atom *argv)
+{
+    utils_anythingToList (cast_pd (x), (t_listmethod)value_list, s, argc, argv);
 }
 
 // -----------------------------------------------------------------------------------------------------------
@@ -124,7 +169,8 @@ static t_buffer *value_functionData (t_gobj *z, int flags)
     
     buffer_appendSymbol (b, sym__restore);
     buffer_appendSymbol (b, x->x_name);
-    buffer_appendFloat (b,  *x->x_raw);
+    listinlet_listGet (x->x_raw, b);
+    buffer_invalidatePointers (b);
     
     return b;
     //
@@ -143,10 +189,10 @@ static void value_restore (t_value *x, t_symbol *s, int argc, t_atom *argv)
     
     if (!old) {     /* Restore the value if it is the only client. */
     //
-    if (x->x_raw && valuecommon_count (x->x_name) == 1) {
-    //
-    value_float (x, atom_getFloatAtIndex (1, argc, argv));
-    //
+    if (argc > 1) {
+        if (x->x_raw && valuecommon_count (x->x_name) == 1) {
+            listinlet_listSet (x->x_raw, argc - 1, argv + 1);
+        }
     }
     //
     }
@@ -161,7 +207,7 @@ static void *value_new (t_symbol *s)
     t_value *x = (t_value *)pd_new (value_class);
     
     x->x_name   = &s_;
-    x->x_outlet = outlet_newFloat (cast_object (x));
+    x->x_outlet = outlet_newAnything (cast_object (x));
     
     value_set (x, s);
     
@@ -197,7 +243,8 @@ void value_setup (void)
     class_addCreator ((t_newmethod)value_new, sym_v, A_DEFSYMBOL, A_NULL);
     
     class_addBang (value_class, (t_method)value_bang);
-    class_addFloat (value_class, (t_method)value_float);
+    class_addList (value_class, (t_method)value_list);
+    class_addAnything (value_class, (t_method)value_anything);
     
     class_addMethod (value_class, (t_method)value_set,      sym_set,        A_DEFSYMBOL, A_NULL);
     class_addMethod (value_class, (t_method)value_restore,  sym__restore,   A_GIMME, A_NULL);
